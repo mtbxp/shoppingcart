998d087475a9bb9a78a101b01b9165acda3c7657
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-06-03 16:02:20 -0300

    Requisito 1 iniciado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,14 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
+  test('Testa se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  test('Ao executar a função fetchProducts com o argumento (computador) fetch deve ser chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  })
+
   fail('Teste vazio');
 });
