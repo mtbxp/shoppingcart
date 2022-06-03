bff89f46b34f278c65cbb99758485ecd524aba26
Roberto <robertog.adm@gmail.com>
2022-06-03 17:21:22 -0300

    Testando se os 25% do requisito 1 ja bateu

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,10 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  
-  fail('Teste vazio');
+  test('testa de fetchSimulator é uma funão', () => {
+    expect(typeof fetchItem()).toBe(Function);
+  })
+  test('Testa se quando passado computador o fetch é chamado', () => {
+    expect(fetchItem('computador')).toHaveBeenCalled();
+  })
 });
