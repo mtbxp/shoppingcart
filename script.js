ca697cced0012ea3d1a1b5d7718485625e82e4d3
Bambirra <ivan.bambirra@engenharia.ufjf.br>
2022-06-03 16:02:49 -0300

    teste 100% fetchProducts

--- script.js
+++ script.js
@@ -12,30 +12,30 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-// const createProductItemElement = ({ sku, name, image }) => {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+const createProductItemElement = ({ sku, name, image }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// };
+  return section;
+};
 
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-// const cartItemClickListener = (event) => {
-//   // coloque seu código aqui
-// };
+const cartItemClickListener = (event) => {
+  // coloque seu código aqui
+};
 
-// const createCartItemElement = ({ sku, name, salePrice }) => {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// };
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
 
 window.onload = () => { };
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,10 +1,13 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
+const messageError = new Error('Você deve fornecer um url');
 
-describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  it ('É uma função', () => {
-    expect(typeof fetchProducts).toBe('function');
+describe('1 - Teste a função fetchProducts', () => {   
+  it('Testa se fetchProducts é function', () => expect(typeof fetchProducts).toBe('function'));
+
+  it('Testa se fethProducts recebe computer como argumento e se foi usado', async () => {    
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 });
