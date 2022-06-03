4afbe92932be27de28f1ae90505074c7b3028ce5
Kauan <kauan.s.leite@hotmail.com>
2022-06-03 18:15:17 -0300

    1° Requisito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,32 @@
-const fetchProducts = () => {
+// const fetch = require('node-fetch')
+
+const selectDados = async (data) => {
+  const teste = { site_id: data.site_id, 
+    query: data.query, 
+    paging: data.paging,
+    results: data.results, 
+    secondary_results: data.secondary_results,
+    related_results: data.related_results, 
+    sort: data.sort, 
+    available_sorts: data.available_sorts,
+    filters: data.filters, 
+    available_filters: data.available_filters,
+  };
+  return teste;
+};
+
+const fetchProducts = async (product) => {
   // seu código aqui
+  if (product === undefined) {
+    return 'You must provide an url';
+  }
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+    const result = await fetch(url);
+    const data = await result.json();
+
+    const teste = selectDados(data);
+
+    return teste;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,28 @@
 require('../mocks/fetchSimulator');
+
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  test('se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  test('deve chamar a funcao fetch em algum momento', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  test('se o endpoint do argumento computador está correto', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(endpoint);
+  })
+  test('se a estrutura de dados retornada é igual ao objeto de computador search', async () => {
+    const data = await(fetchProducts('computador'));
+    expect(Object.keys(data)).toEqual(Object.keys(computadorSearch))
+  })
+  test('se a url estiver vazia retorna um erro', async () => {
+    expect(await fetchProducts()).toBe('You must provide an url');
+  })
 });
