4cc393776728d5790f7b00b0ae73a086c7778de0
leovcampos <leonardoviniciuscampos@gmail.com>
2022-06-03 19:23:30 -0300

    requisito 6 e 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+    it('localStorage.setItem is called', () => {
+        getSavedCartItems();
+        expect(localStorage.getItem).toBeCalled();
+    });
+    it('localStorage.setItem is called', () => {
+        getSavedCartItems();
+        expect(localStorage.getItem).toBeCalledWith('cartItems');
+      });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,12 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('localStorage.setItem is called', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('localStorage.setItem is called', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
