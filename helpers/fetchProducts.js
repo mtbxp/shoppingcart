eaa8fecdfe6da53f420e738a326785d4cf42e850
Jonas Vasconcelos <jonas.vasconcelos.filho@gmail.com>
2022-06-03 16:19:05 -0300

    iniciando projeto

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+
+  await fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => `Algo deu errado :( \n${error}`);
 };
+
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
