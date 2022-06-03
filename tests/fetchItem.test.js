5fe62d8cb15d990e92500aa5ffc5a81c943f4a47
Dimy Leo <leonardoprimoc@hotmail.com>
2022-06-03 19:02:12 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,19 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  })
+  it('Testa se feath foi chamado com argumento MLB1615760527', async () => {
+    await fetchItem("MLB1615760527");
+    expect(fetch).toBeCalledWith("https://api.mercadolibre.com/items/MLB1615760527");
+  })
+  it('Testa se ao argumento MLB1615760527 retorna uma estrutura igual ao item', async () => {
+    const resp = await fetchItem("MLB1615760527")
+    expect(resp).toEqual(item);
+  })
+  it('Sem argumento retorna erro', async () => {
+    const resp = await fetchItem()
+    expect(resp).toEqual(new Error('You must provide an url'));
+  })
 });
