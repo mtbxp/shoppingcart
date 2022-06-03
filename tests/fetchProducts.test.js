5d7415a261c5a4aaed9d0630030b1d34b658d68e
Felipe <fe.donatto@outlook.com>
2022-06-03 15:48:54 -0300

    1. (TDD) Desenvolva testes de no mínimo 25% de cobertura total e 100% da função fetchProducts finalizado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,24 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  })
+  it('Chama o fetch', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('chama o fetch com o endpoint da API mercado livre', () => {
+    fetchProducts('computador');
+    expect(fetch()).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  })
+  it('retorna o objeto esperado', () => {
+    expect(fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  it('retorna o objeto esperado', () => {
+    expect(()=> {
+      fetchProducts();;
+    }).toThrow(new Error('You must provide an url'))
+  })
+
 });
