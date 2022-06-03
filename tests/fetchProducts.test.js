d6f162d8bcc7064fb61ba25cfe39733f2eb1fc47
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-03 15:25:35 -0300

    Testando a função se a fetch foi chamada e se está utilizando o endpoint.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,13 @@ describe('1 - Teste a função fetchProducts', () => {
 test('Testando se fetchProducts é uma função', () => {
   expect(typeof fetchProducts).toEqual('function');
 });
+test('Passando "computador" como parâmetro, verificar se a fetch foi chamada', async () => {
+  await fetchProducts('computador');
+  expect(fetch).toHaveBeenCalled();
+});
+test('Passando o argumento "computador", se a fetch utiliza o endpoint', async () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  await fetchProducts('computador');
+  expect(fetch).toHaveBeenCalledWith(endpoint);
+})
 });
