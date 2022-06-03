6c0004b1f0e7cfef82f2c65fe64ea25e22516e9c
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 17:02:55 -0300

    refatorando a função

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,12 @@
 // const fetchProductsUrl = (product) =>
 //   `https://api.mercadolibre.com/sites/MLB/search?q=$QUERY/${product}`;
 
-const fetchProducts = async () => {
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+const fetchProducts = async (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
   
   const result = await fetch(url)
     .then((response) => response.json())
-    .then((data) => data.data)
+    // .then((data) => data.)
     .catch((error) => `You must provide an url \n${error}`);
   return result;
 };
