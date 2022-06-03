477ef72ec814b2e88e7d15722413a8a94e073797
Larissa <larissamf25@gmail.com>
2022-06-03 16:11:20 -0300

    requisito 2 (não passando)

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+// Project <Pixels art> from <Larissa Menezes> done in 22.06.03 for the Trybe course, ninth week. It has been used as reference the notes from the class an external links indicated along the code line
+
+const fetch = require('node-fetch');
+
+const getURL = (product) => `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+
+const fetchProducts = async (productName) => {
+  const response = await fetch(getURL(productName));
+  const data = await response.json();
+  const list = data.results;
+  return list;
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Project <Pixels art> from <Larissa Menezes> done in 22.06.03 for the Trybe course, ninth week. It has been used as reference the notes from the class an external links indicated along the code line
 
+const { create } = require("mochawesome-report-generator");
+const { fetchProducts } = require("./helpers/fetchProducts");
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -17,13 +20,23 @@ const createCustomElement = (element, className, innerText) => {
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
+};
+
+const createProductList = () => {
+  const listOfProducts = fetchProducts('computador');
+  listOfProducts.forEach((product) => {
+    const productObject = {
+      sku: product.id,
+      name: product.title,
+      image: product.thumbnail,
+    };
+    createProductItemElement(productObject);
+  });
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
@@ -40,4 +53,4 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => { createProductList(); };
