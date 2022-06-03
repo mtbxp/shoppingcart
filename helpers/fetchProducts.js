989ed25768d5db49bf6b891a9eb843a3e32cee94
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 15:56:22 -0300

    teste passando argumento e retornando fetch

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProductsUrl = (product) =>
+  `https://api.mercadolibre.com/sites/MLB/search?q=$QUERY/${product}`;
+
+const fetchProducts = async (product) => {
+  try {
+    const url = fetchProductsUrl(product);
+    const response = await fetch(url);
+    const data = await response.json();
+  } catch (error) {
+    return error;
+  } 
 };
 
 if (typeof module !== 'undefined') {
