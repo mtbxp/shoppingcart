55263f938498d788c0860dc001e2a95b07b7753d
Diogo Camilo <diogocgp@icloud.com>
2022-06-03 18:07:31 -0300

    requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,20 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts ', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it ('Retorna se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Testa se fetch foi chamado quando passado o argumento "computador" para função fetchProducts', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador" , a função fetch utiliza o endpoint " https://api.mercadolibre.com/sites/MLB/search?q=computador "', async () => {
+    const apiEndPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(apiEndPoint);
+  });
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    await expect(fetchProducts('computador')).toEqual(computadorSearch);
+  });
 });
