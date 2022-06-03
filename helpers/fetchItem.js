b79e2d8ce41fb17522bd65ee202ddfd7579d3286
Emily Menezes <estudosmenezes@gmail.com>
2022-06-03 17:35:03 -0300

    fetchItem.js

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (flag) => {
+  const getUrl = `https://api.mercadolibre.com/items/${flag}`;
+    const getResponse = await fetch(getUrl).then((response) => response.json())
+    .then((date) => date.results)
+    .catch((error) => error);
+    return getResponse;
 };
 
 if (typeof module !== 'undefined') {
