0a0eb3f2519854bc8b9bc6e790130f9ef08a8878
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-06-03 16:10:18 -0300

    Requisito 1 atualizado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -10,6 +10,12 @@ describe('1 - Teste a função fetchProducts', () => {
   test('Ao executar a função fetchProducts com o argumento (computador) fetch deve ser chamada', async () => {
     await fetchProducts('computador');
     expect(fetch).toBeCalled();
+  });
+
+  test('Ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint (https://api.mercadolibre.com/sites/MLB/search?q=computador)', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(endpoint);
   })
 
   fail('Teste vazio');
