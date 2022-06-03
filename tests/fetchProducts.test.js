12c20e46f1aa8a3839b65bc7be4293c3e60ad4d9
Icaro Joel <icaromoura.programador@gmail.com>
2022-06-03 16:02:48 -0300

    req 1 - task 1/5

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,10 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('test the function fetchProducts', () => {
+  it('a function', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  
 });
