2ea39c648dae250198f32f2a11155649e8b11918
Miyuki Ishii <miyukisabrina@gmail.com>
2022-06-03 17:45:06 -0300

    Requisito 2 - Criando listagem de produto

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  try {
+    const products = await fetch(url)
+    .then((object) => object.json());
+    return products;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,6 +22,15 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
+};
+
+const createListOfProducts = async () => {
+  const items = document.getElementsByClassName('items')[0];
+  const products = await fetchProducts('computador')
+  .then((object) => object.results);
+  products.forEach((item) => {
+    items.appendChild(createProductItemElement(item));
+  });
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
@@ -38,4 +47,4 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => { createListOfProducts(); };
