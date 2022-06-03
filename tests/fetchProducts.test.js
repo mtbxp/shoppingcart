a831a3a9df9b48f55499273c5021a585964dee9b
Andre <andre09999@live.com>
2022-06-03 16:11:31 -0300

    exercicio 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,28 @@
-require('../mocks/fetchSimulator');
+const fetchSimulator = require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('01-Testar se e uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('02-testar se com argumento computador chama fetch', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  it('03-testar se com argumento computador chama fetch',async () => {
+    fetchProducts('computador');
+   await expect(fetch).toHaveBeenCalled();
+  });
+  it('04- se a função e igual a computador search', () => {
+    const expec = fetchProducts('computador')
+    const retorno = computadorSearch
+    expect(expec).toEqual(retorno)
+  })
+  it('05-Se chamar a função sem parametro retorna erro', () => {
+    const func = fetchProducts('');
+    const msg = 'You must provide an url'
+    expect(func).toBe(new console.error(msg))
+  })
 });
