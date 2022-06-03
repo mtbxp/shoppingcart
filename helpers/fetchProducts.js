1c54825a6d74c49d632e2aed61f504137b170cdf
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-03 18:57:21 -0300

    Tentando terminar a função para o 2º requisito.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,26 @@
-const fetchProducts = () => {
-  // seu código aqui
+const getUrl = (QUERY) => `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+
+const fetchProducts = async (product) => {
+const url = getUrl(product);
+const result = await fetch(url);
+const data = await result.json();
+const objectData = data.results;
+const returnData = objectData.map((element) => element);
+const object = returnData.foreach((element) => element);
+// const { id: sku, title: name, thumbnail: image } = object;
+// const { id: sku, title: name, thumbnail: image } = objectData;
+// const resultObject = objectData.forEach((element) => element);
+return console.log(object);
+/* const { id: sku, title: name, thumbnail: image } = resultObject;
+return {
+  sku,
+  name,
+  image,
+}; */
 };
+
+// console.log(fetchProducts('computador').then((data) => data));
+fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
