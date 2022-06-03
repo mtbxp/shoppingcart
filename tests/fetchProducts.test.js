bdff2a71641eb771978a7d40f933ca82458e6b19
Malrus-dev <maelsobfer@gmail.com>
2022-06-03 17:06:08 -0300

    requisito 1, testando a url

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,10 +6,14 @@ describe('1 - Teste a função fetchProducts', () => {
   test('se é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
-  test('se ao chamar com o argumento "computador", o fetch foi chamado', () => {
-    const fetch = jest.fn(() => true );
-    fetchProducts(fetch(), 'computador');
-    expect(fetch).toHaveBeenCalledTimes(1);
+  test('se ao chamar com o argumento "computador", o fetch foi chamado', async () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
   });
-  
+  test(`passando o argumento "computador", a função fetch utiliza o endpoint
+  "https://api.mercadolibre.com/sites/MLB/search?q=computador"`, async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    fetchProducts('computador');
+    expect(fetch).toHaveReturnedWith(url);
+  });
 });
