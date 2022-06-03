c81e8143e27dbdb57cbd45140967cba68e5b6feb
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-06-03 16:34:18 -0300

    implemented fetchProduct function and test cases

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,8 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  if (product && typeof product !== 'string') throw new Error('O parâmetro deve ser uma string');
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,38 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchProducts foi definido', () => {
+    expect(fetchProducts).toBeDefined();
+  });
+
+  it('Verifica se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Verifica se a função retorna um erro caso o parâmetro passado não seja uma string', async () => {
+    const notAString = 1;
+    await expect(fetchProducts(notAString)).rejects.toThrow(new Error('O parâmetro deve ser uma string'));
+  });
+
+  it('Verifica que se nenhum parâmetro for passado para a função, é lançado um erro', async () => {
+    await expect(fetchProducts()).rejects.toThrow(new Error('You must provide an url'));
+  });
+
+  it('Verifica se ao passar um parâmetro válido para a função a função fetch é chamada', async () => {
+    const validParam = 'computador';
+    await fetchProducts(validParam);
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se a função fetch utiliza o endpoint correto', async () => {
+    const validParam = 'computador';
+    const expectedEndPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts(validParam);
+    expect(fetch).toHaveBeenCalledWith(expectedEndPoint);
+  });
+
+  it('Verifica se o retorno da função é um objeto com a estrtura igual ao esperado', async () => {
+    const validParam = 'computador';
+    await expect(fetchProducts(validParam)).resolves.toEqual(computadorSearch);
+  });
 });
