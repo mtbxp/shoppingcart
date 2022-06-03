2e538cecda38a8adcf73b4bca202b54aa244a17d
Iago <iagoassis21@outlook.com>
2022-06-03 19:22:11 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  if (item === undefined) { return Promise.reject(new Error('You must provide an url')); }
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,5 +3,29 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it(`O window.fetch está definido em todos os testes, ou seja, será possível usar a função fetch dentro do seu ambiente de testes sem precisar importar ou instalar bibliotecas`, async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(url);
+  })
+  it(`Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, 
+  que já está importado no arquivo.`, async () => {
+    const result = await fetchProducts('computador');
+  expect(result).toEqual(computadorSearch);
+  })
+  it(`Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: 'You must provide an url'`, async () => {
+    try {
+      await fetchProducts();
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  })
 });
