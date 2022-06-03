ce16db1831b9c23c8af701f08254058f6854e94f
Pedro Papini <ppapinia@gmail.com>
2022-06-03 16:29:49 -0300

    Retira try/catch de fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,13 +1,9 @@
 const fetchProducts = async (query) => {
   if (query === undefined) { throw new Error('You must provide an url'); }
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  try {
     const response = await fetch(endpoint);
     const json = await response.json();
     return json.results;
-  } catch (err) {
-      console.log(err);
-  }
 };
 
 if (typeof module !== 'undefined') {
