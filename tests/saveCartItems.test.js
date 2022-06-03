ed2b20d70ca130fcee019ad9e667eee45cfe5338
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-06-03 17:46:14 -0300

    saveCartItems function and test cases implemented

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,23 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se saveCartItems foi definida', () => {
+    expect(saveCartItems).toBeDefined();
+  });
+
+  it('Verifica se saveCartItems é uma função', () => {
+    expect(typeof saveCartItems).toBe('function');
+  });
+
+  it('Verifica se quando a função é chamada com um parâmetro válido o método do localStorage `setItem` é chamado', () => {
+    const validParam = '<ol><li>Item</li></ol>';
+    saveCartItems(validParam);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Verifica se quando a função é chamada com um parâmetro válido o método do localStorage `setItem` é chamado com os parâmetros corretos', () => {
+    const validParam = '<ol><li>Item</li></ol>';
+    saveCartItems(validParam);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', validParam);
+  });
 });
