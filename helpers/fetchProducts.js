48faba61212bb498730349ab2213ecd0b0f8ad17
Emily Menezes <estudosmenezes@gmail.com>
2022-06-03 16:28:36 -0300

    Requisito 2 e 3

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (flag) => {
+    const getUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${flag}`;
+    const getResponse = await fetch(getUrl).then((response) => response.json())
+    .then((date) => date.results)
+    .catch((error) => error);
+    return getResponse;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,8 +1,29 @@
 require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
+const errorFetchProducts = new Error('You must provide an url');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Test if fethItem is a function', () => expect(typeof fetchItem).toBe('function'));
+
+  it('Test if the return of the fethItem function with the argument "MLB1615760527" is a data structure equal to the item object, which is already imported in the file', async () => {    
+    const expectArgument = await fetchItem('MLB1615760527');
+    expect(expectArgument).toMatchObject(item);
+  });
+
+  it('Test if calling the fethItem function with no argument returns an error with the message: You must provide an url', async () => {    
+    const errorFetchItem = await fetchItem();
+    expect(errorFetchItem).toEqual(errorFetchProducts);
+  }); 
+
+  it('Test fetchItem function with argument MLB1615760527 and test if fetch was called', async () => {    
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Test if when calling the fetchItem function with the argument "MLB1615760527", the fetch function uses the endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador', async () => {    
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
 });
