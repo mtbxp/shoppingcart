76129202220001948691edb1ceddf6796aed7d6d
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 18:49:48 -0300

    implementando a função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItemUrl = (idItem) => `https://api.mercadolibre.com/items/${idItem}`;
+
+const fetchItem = async (idItem) => {
+  try {
+    const url = fetchItemUrl(idItem);
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
