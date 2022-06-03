70c3b86e79609e302191d862f81e15045ed64cb8
Bambirra <ivan.bambirra@engenharia.ufjf.br>
2022-06-03 16:33:44 -0300

    100% fetchItem req2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,8 +1,14 @@
 require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
+const messageError = new Error('Você deve fornecer um url');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchItem é funçao', () => {
+    expect(typeof fetchItem).tobe('function');
+  });
+  it('Testa se fetchItem recebe "MLB1615760527" e tem endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async() => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
