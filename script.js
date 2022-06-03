a3aeee1597cd08ea03c7de4b5c5bc0fdb20cc2e5
Ranieri Fernandes <ranierirfm@hotmail.com>
2022-06-03 18:16:49 -0300

    Requisitos 1 e 2

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+// Cria um elemento de imagem:
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 };
-
+// .........................................................................................
+// Estrutura para criar um elemento:
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 };
-
+// .........................................................................................
+// Cria a lista de produtos:
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -23,13 +26,16 @@ const createProductItemElement = ({ sku, name, image }) => {
 
   return section;
 };
-
+// .........................................................................................
+// Pega o id de um produto:
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
+// .........................................................................................
+// Escuta a ação de clicar em um item no carrinho:
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
 };
-
+// .........................................................................................
+// Cria os elementos do carrinho:
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -37,5 +43,16 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.addEventListener('click', cartItemClickListener);
   return li;
 };
+// .........................................................................................
+// Minha função do Requisito 1 que recebe e saída do fetch trabalha ela e o resultado usao como parâmetro da função "createProductItemElement".
+const filterData = async () => {
+const item = document.querySelector('.items');
+const data = await fetchProducts('computador');
+data.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
+  item.appendChild(createProductItemElement({ sku, name, image }));
+});
+}; 
 
-window.onload = () => { };
+window.onload = () => { 
+  filterData();
+};
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,19 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se fetchProducts é uma função', ()=>{
+    expect(typeof fetchProducts).toBe('function');
+  })
+  test('se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint correto', async () => {
+    await fetchProducts('computador')
+    const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toBeCalledWith(endPoint);
+  })
+  test('se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  test('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const erro = new Error ('You must provide an url')
+    expect(await fetchProducts()).toEqual(erro);
+  })
 });
