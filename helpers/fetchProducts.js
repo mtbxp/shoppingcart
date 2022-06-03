3ef40b8999d659f80a106a3f5e1377e8071745df
Marcelo Ataíde <msa.trybe@gmail.com>
2022-06-03 17:41:53 -0300

    Requisito 1.2 - Deveria ser capaz de invocar a função `fetch`

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui
+const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
+const createUrlToGetProductsList = (queryTerm) => `${BASE_URL}/search?q=${queryTerm}`;
+
+const fetchProducts = async (queryTerm) => {
+  const url = createUrlToGetProductsList(queryTerm);
+  await fetch(url);
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,6 +4,14 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   it('Deveria falhar se fetchProducts não for uma função', () => {
+    expect.assertions(1);
     expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Deveria ser capaz de invocar a função fecth.', async () => {
+    expect.assertions(1);
+    const queryTerm = 'computador';
+    await fetchProducts(queryTerm);
+    expect(fetch).toHaveBeenCalled();
   });
 });
