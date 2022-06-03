e45d75fd392df8b4807a3b5f29af198c710419b6
João Mattedi <joaommattedi@gmail.com>
2022-06-03 16:00:23 -0300

    requisito 3 TDD, implementada a funcao fetchItems

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,4 +5,21 @@ const item = require('../mocks/item');
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
   // fail('Teste vazio');
+  it('deve ser um função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+  it('deve passar o argumento MLB1615760527 chamar o fetch', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('deve chamar o fetch com o link da api como argumento', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('deve retornar uma estrutura de dados igual ao computadorSearch', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  })
+  it('caso nenhum parametro seja passado, deve retornar um erro com a mensagem: You must provide an url', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  });
 });
