6c382acfb8992feb5a8e45cd9851c6352e35512a
danielrubens <danielrubensdaniel@gmail.com>
2022-06-03 20:45:19 -0300

    01

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (arg) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${arg}`;
+  const responseData = fetch(url)
+  .then((response) => response.json())
+  .then((data) => data);
+
+  return responseData;
 };
 
 if (typeof module !== 'undefined') {
