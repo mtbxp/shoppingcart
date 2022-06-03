ae2aea740bdec01a646a682568161b663d550de8
Larissa <larissamf25@gmail.com>
2022-06-03 16:23:08 -0300

    requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,22 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
+  it('should be a function', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('should have called fetch', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('should uses an specific endpoint when calling fetch', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('should return the correct item', async () => {
+    expect(fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('should return an error when called without arguments', async () => {
+    expect(fetchItem()).toEqual(new Error ('You must provide an url'));
+  });
   fail('Teste vazio');
 });
