03263b1a57be8aa885b345a02c8a34c2eafe068a
Artur Vidor <vidorartur@gmail.com>
2022-06-03 17:01:35 -0300

    primeiro exercicio OK

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,18 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+      // seu código 
+      
+        const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+        if (item === undefined) {
+          return 'You must provide an url';
+        }
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return data;
 };
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    fetchProducts,
-  };
-}
+      if (typeof module !== 'undefined') {
+        module.exports = {
+          fetchProducts,
+        };
+      }
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,22 @@
 require('../mocks/fetchSimulator');
-const { fetchProducts } = require('../helpers/fetchProducts');
+const {
+  fetchProducts
+} = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se fetchProducts é uma função', async () => {
+    await expect(typeof fetchProducts).toBe('function');
+  })
+  test('Se quando o argumento é \'computador\' o fecth retorna a URL certa', async () => {
+    await fetchProducts('computador');
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toBeCalledWith(url);
+  })
+  test('Se quando o argumento é \'computador\' retorna um arquivo igual ao esperado', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  test('Deve retornar a mensagem \'You must provide an url\' quando a função é chamda sem argumento', async () => {
+    expect(await fetchProducts()).toBe('You must provide an url');
+  })
 });
