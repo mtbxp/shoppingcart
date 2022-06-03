df36d1a79d37b93a2dd9971709eeb15532813566
edmcorrea <edm.correa@hotmail.com>
2022-06-03 16:52:55 -0300

    adicionando quesito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,35 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect( typeof fetchProducts).toBe('function');
+  })
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  })
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toHaveBeenCalledWith(computadorSearch);
+  })
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    try {
+      await fetchProducts();
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBe('You must provide an url');
+    }
+    const result = 
+    expect(result).toHaveBeenCalledWith(computadorSearch);
+  })
+  // fail('Teste vazio');
 });
+
+// it('Teste se fetchProducts é uma função', async () => {
+    
+// })
