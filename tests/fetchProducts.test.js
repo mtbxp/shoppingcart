69fdf4fbf9996dd6f909b432534d9dbdc9d386d2
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 15:46:47 -0300

    implementando teste com argumento computador

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,7 +3,14 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  it('Verifica se fetchProducts é uma função', () => {
+  it('2 - Verifica se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
+
+  it('3 - Com o argumento "computador", a função fetch deve ser chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('4 - Com o argumento "computador", a função fetch deve ser chamada')
 });
