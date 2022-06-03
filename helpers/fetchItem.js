d52f4c7dd302e888080175708ada6fb3b0a3e668
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-06-03 17:04:44 -0300

    fetchItem function and test cases implemented

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (productId) => {
+  if (productId && typeof productId !== 'string') {
+    throw new Error('O parâmetro passado deve ser uma string');
+  }
+  const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,38 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem foi definindo', () => {
+    expect(fetchItem).toBeDefined();
+  });
+
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Verifica se é lançado uma exceção quando o parâmetro passado não é uma string', async () => {
+    const notAString = 1233;
+    await expect(fetchItem(notAString)).rejects.toThrow(new Error('O parâmetro passado deve ser uma string'));
+  });
+
+  it('Verifica se quando um parâmetro válido é passado a função fetch é chamada', async () => {
+    const validParam = 'MLB1615760527';
+    await fetchItem(validParam);
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se quando um parâmetro válido é passado a função fetch utiliza o endpoint correto', async () => {
+    const validParam = 'MLB1615760527';
+    const expectedEndpoint = `https://api.mercadolibre.com/items/${validParam}`;
+    await fetchItem(validParam);
+    expect(fetch).toHaveBeenCalledWith(expectedEndpoint);
+  });
+
+  it('Verifica se nenhum parâmetro for passado é lançado uma exceção', async () => {
+    await expect(fetchItem()).rejects.toThrow(new Error('You must provide an url'));
+  });
+
+  it('Verifica se o retorno da função é um objeto com a estrutura correta', async () => {
+    const validParam = 'MLB1615760527';
+    await expect(fetchItem(validParam)).resolves.toEqual(item);
+  });
 });
