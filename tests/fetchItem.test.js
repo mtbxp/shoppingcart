ccdd536fa31d88ab3451bb0bb3c8374f09a50100
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 19:23:30 -0300

    teste retorna obj correto

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -9,17 +9,20 @@ describe('2 - Teste a função fetchItem', () => {
 
   it('4 - Com o argumento "MLB1615760527", a função fetch é chamada', async () => {
     await fetchItem('MLB1615760527');
+    
     expect(fetch).toHaveBeenCalled;
   });
 
   it('5 - Com o argumeno "MLB1615760527", a função fetch deve chamar o endpoint correto', async () => {
     const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527'
     await fetchItem('MLB1615760527');
+
     expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
   it('6 - Deve retornar um objeto com as propriedades esperadas', async () => {
     const response = await fetchItem('MLB1615760527');
+
     expect(response).toEqual(item);
   })
 });
