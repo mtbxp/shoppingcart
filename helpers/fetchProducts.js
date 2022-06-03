b78f7feae8eeeb3ace8dd01c8326e7e9e9d3cc52
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-06-03 16:26:11 -0300

    Requisito 1.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const getFetchProductURL = (id) => `https://api.mercadolibre.com/sites/MLB/search?q=${id}`;
+
+const fetchProducts = async (id) => {
+  try {
+    const url = getFetchProductURL(id);
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,31 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('verifique se, ao chamar a função fetchProducts com o argumento "computador", a função fetch foi chamada.', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador.', async () => {
+
+    const response = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(response);
+
+  });  
+
+  it('verifique se, ao chamar a função fetchProducts com o argumento "computador", a função fetch foi chamada.', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+
+  it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
+    const failRequest = await fetchProducts();
+    expect(failRequest).toEqual(new Error('You must provide an url'));
+  });
 });
