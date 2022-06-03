f47962e00c02d4ac940e83e34e4a7ec90adddf8d
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-03 15:33:20 -0300

    Adicionando testes: retorno igual ao objeto de acordo com o argumento passado e se retorna um erro se for chamada a função sem nenhum argumento.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -14,5 +14,13 @@ test('Passando o argumento "computador", se a fetch utiliza o endpoint', async (
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   await fetchProducts('computador');
   expect(fetch).toHaveBeenCalledWith(endpoint);
-})
+});
+test('Testando se o retorno da função fetchProducts com o argumento "computador" tem estrutura de dados igual ao objeto computadorSearch', async () => {
+  const expect = await fetchProducts('computador');
+  expect(() => expect).toEqual(computadorSearch);
+});
+test('Ao chamar a função fetchProducts sem argumento, retorna um erro', async () => {
+  const expect = fetchProducts();
+  expect(expect).toEqual(new Error('You ,ust provide an url'));
+});
 });
