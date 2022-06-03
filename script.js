34458a017c549d061db72f37d2d1cda759e9a47d
Breno Nascimento <satsukebare23@gmail.com>
2022-06-03 19:05:11 -0300

    adicionando requisito 4

--- script.js
+++ script.js
@@ -5,28 +5,6 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
-const createCustomElement = (element, className, innerText) => {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-};
-
-const createProductItemElement = ({ sku, name, image }) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  const componentPai = document.querySelector('.items');
-  componentPai.appendChild(section);
-};
-
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
 };
@@ -36,8 +14,45 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  const componentePai = document.querySelector('.cart__items');
+  componentePai.appendChild(li);
   return li;
 };
+
+const createCustomElement = (element, className, innerText) => {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+};
+
+const getProduct = (id) => fetchItem(id).then((produto) => {
+  const baseObj = {
+    sku: produto.id,
+    name: produto.title,
+    preco: produto.price,
+  };
+  createCartItemElement(baseObj);
+});
+
+const createProductItemElement = ({ sku, name, image }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.addEventListener('click', (event) => {
+    const father = event.target.parentNode;
+    const childrenValue = father.children[0].innerText;
+    getProduct(childrenValue);
+  });
+  const componentPai = document.querySelector('.items');
+  componentPai.appendChild(section);
+};
+
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 fetchProducts('computador').then((item) => {
   item.forEach((produto) => {
