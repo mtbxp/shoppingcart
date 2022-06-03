bad43a353801bdcc60255d8e1a365ac09a804d8c
Diogo Camilo <diogocgp@icloud.com>
2022-06-03 19:02:22 -0300

    requisito 1 correções

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,15 +8,20 @@ describe('1 - Teste a função fetchProducts ', () => {
     expect(typeof fetchProducts).toBe('function');
   });
   it('Testa se fetch foi chamado quando passado o argumento "computador" para função fetchProducts', async () => {
-    await fetchProducts('computador');
+    const fetch = jest.fn();
+    await fetchProducts(fetch(), 'computador');
     expect(fetch).toBeCalled();
   });
   it('Teste se, ao chamar a função fetchProducts com o argumento "computador" , a função fetch utiliza o endpoint " https://api.mercadolibre.com/sites/MLB/search?q=computador "', async () => {
     const apiEndPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    const fetch = jest.fn();
     await fetchProducts('computador');
-    expect(fetch).toBeCalledWith(apiEndPoint);
+    expect(fetch()).toBeCalledWith(apiEndPoint);
   });
   it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
     await expect(fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    await expect(fetchProducts()).toEqual(new Error('You must provide an url'));
   });
 });
