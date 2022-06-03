cc47c407cae6c26ca030fbdae0121ea3f6c422c0
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 16:37:35 -0300

    teste passando argumento e chamando endpoint

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -13,9 +13,14 @@ describe('1 - Teste a função fetchProducts', () => {
   });
 
   it('4 - Com o argumento "computador", a função fetch deve ser chamada com endpoint correto', async () => {
-    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY/computador';
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
     await fetchProducts('computador');
 
     expect(fetch).toHaveBeenCalledWith(endpoint);
   })
+
+  it('5 - Deve retornar um objeto com as propriedades esperadas', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
 });
