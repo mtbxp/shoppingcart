7ca03f881dab98cd4c5de64a3a2e95fb95aaed5c
Icaro Joel <icaromoura.programador@gmail.com>
2022-06-03 20:40:57 -0300

    req 6 - task 2/2

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,13 @@ describe('test the function saveCartItems', () => {
   it('when executing saveCartItems, method [] is called', async () => {
     await saveCartItems('<ol><li>Item</li></ol>');
 
-    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('when executing saveCartItems, method [] is called', async () => {
+    const arg = '<ol><li>Item</li></ol>';
+    await saveCartItems(arg);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', arg);
   });
 });
