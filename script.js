0d7c32bb0a058a3e9403d3425a0fc67ccb9f6115
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-06-03 14:53:25 -0300

    Primeiro commit do Shopping Cart

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+  // modificando para o primeiro commit
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
