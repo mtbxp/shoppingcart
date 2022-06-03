09d9bc9040906740e08abe8d11c6b6d79d473070
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-06-03 15:15:34 -0300

    Fiz o requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,20 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts deve ser uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  })
+  it('A função fetch deve ser chamada com o endpoint esperado ao ser passado "computador" como argumento', () => {
+    fetchProducts('computador');
+    const expectedEndPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toBeCalled();
+    expect(fetch).toBeCalledWith(expectedEndPoint);
+  })
+  it('fetchProducts retorna o objeto esperado', async () => {
+    expect(await fetchProducts()).toEqual(computadorSearch);
+  })
+  it('fetchProducts deve retornar um erro ao não receber argumento', async () => {
+    expect(fetchProducts()).rejects.toMatch('Error');
+  })
+  
 });
