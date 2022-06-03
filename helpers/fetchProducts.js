450d06455e89f31f3838a2f25402e247be6df0d2
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-06-03 16:35:46 -0300

    2. fetchProducts começado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,26 +1,9 @@
-const pesquisaItem = (item = 'computador') => `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
-
-const fetchProducts = async (items) => {
-  const url = pesquisaItem(items);
+const fetchProducts = async (item) => {;
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
   const resultado = await fetch(url);
   const data = await resultado.json();
-
-  /*
-Implemente a função fetchProducts para retornar a listagem de produtos;
-
-O retorno de produtos se encontra no array results;
-
-Utilize a função createProductItemElement() para criar os componentes HTML referentes a um produto:
-
-Adicione cada elemento retornado da função createProductItemElement(product) como filho do elemento <section class="items">.
-Obs: Utilize as variáveis fornecidas no código, elas devem se referir aos seguintes campos:
-
-sku: é o campo id retornado pela API;
-name: é o campo title retornado pela API;
-image: é o campo thumbnail retornado pela API. */
+  return data;
 };
-
-fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
