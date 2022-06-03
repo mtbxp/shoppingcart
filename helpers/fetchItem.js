e478d5c64670f60bff7f3bd37ee7ab81c585c227
Henos <henosvinicius13@gmail.com>
2022-06-03 17:15:23 -0300

    Resolução do requisito 03

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = () => {
+const fetchItem = async (id) => {
   // seu código aqui
+  const endpoint = `https://api.mercadolibre.com/items/${id}`;
+
+  try {
+    const reponse = await fetch(endpoint);
+    const data = await reponse.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -2,7 +2,28 @@ require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
+const id = 'MLB1615760527';
+
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  test('Se fetch foi chamado junto à fetchItem', async () => {
+    await fetchItem(id);
+    expect(fetch).toHaveBeenCalled();
+  });
+  test('Se ao chamar a fecthItem, fetch deve receber uma url específica', async () => {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem(id);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  test('Se o retorno de fetchItem é um objeto igual a item', async () => {
+    const data = await fetchItem(id);
+    expect(data).toStrictEqual(item);
+  });
+  test('Se, caso fetchItem seja chamado sem argumento, retorna a mensagem de erro', async () => {
+    const data = await fetchItem();
+    expect(data).toEqual(new Error('You must provide an url'));
+  });
 });
