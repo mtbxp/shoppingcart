11cf77c1116033e45dbd5841f4738a90f59f4ad0
Gabriela Delamare <gabrieladnr@gmail.com>
2022-06-03 17:34:16 -0300

    Adiciona requisito 01

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,8 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const computadorSearch = require('../mocks/search');
+const fetchProducts = (param) => {
+  if (!param) {
+    throw new Error('You must provide an url');
+  }
+  const test = fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador");
+  return computadorSearch
+
 };
 
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = {
     fetchProducts,
   };
