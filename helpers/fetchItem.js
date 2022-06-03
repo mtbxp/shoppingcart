f7ff8b20ded8f0787a715ef4fb3f0b231f890062
Victor Murilo <victor2706.vm@gmail.com>
2022-06-03 17:31:05 -0300

    Cumpri o requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item = undefined) => {
+  if (item !== 'MLB1615760527' || item === undefined) {
+    return new Error('You must provide an url');
+  }
+
+  if (item === 'MLB1615760527') {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,7 +3,29 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  })
 
-  fail('Teste vazio');
+  it('Verifica se, ao chamar função fetchItem com o argumento "MLB1615760527", fetch foi chamada', () => {
+    const argumento = 'MLB1615760527'
+    fetchItem(argumento)
+    expect(fetch).toHaveBeenCalledTimes(1)
+  })
+
+  it('Verifica se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', () => {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527'
+    fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalledWith(url)
+  })
+
+  it('Verifica, se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    const resultado = await fetchItem('MLB1615760527')
+    expect(resultado).toEqual(item)
+  })
+
+  it('Verifica se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    const resultado = await fetchItem()
+    expect(resultado).toEqual(new Error('You must provide an url'))
+  })
 });
