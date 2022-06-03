532a5de078e55a0083eacbadb59db086939a9472
Breno Prado <breno.o.prado@hotmail.com>
2022-06-03 18:28:40 -0300

    Requisito 3 completo :turtle:

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,19 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+  test('fetch é chamado na função fetchItem', async () => {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  test('retorno esperado da função', async (done) => {
+    const expected = await fetchProducts();
+    done();
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+    expect(() => expected).toEqual(new Error('You must provide an url'));
+  });
 });
