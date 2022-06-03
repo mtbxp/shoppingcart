b997805fe01a8fb986c76f0bcb94df49121b55a7
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-06-03 19:08:30 -0300

    Exercicio 1 feito, será que está ok? Vamos descobrir :)

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,22 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Check if fetchProducts is a function', () => {
+    expect(fetchProducts).toBeInstanceOf(Object);
+  });
+  it('check if receiving the argument \'computador \' is called the fetch', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('check if you pass the argument \'computador\' the fetch function uses the correct endpoint', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  it('check if you pass the argument \'computador\' a data structure is returned equal to the computer objectsearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('check if by calling the function without argument it returns the error with the message \'You must provide an url\'', async () =>{
+    expect(await fetchProducts()).toThrow('You must provide an url');
+  });
 });
