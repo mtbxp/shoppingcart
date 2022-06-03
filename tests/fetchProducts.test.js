c115db25a4954b0adae1086a139c7b04345791be
João Mattedi <joaommattedi@gmail.com>
2022-06-03 15:11:02 -0300

    requisito 1 TDD

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,22 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('deve ser um função', () => {
+    expect(typeof fetchProducts()).toEqual('function');
+  });
+  it('deve passar o argumento computador chamar o fetch', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('deve chamar o fetch com o link da api como argumento', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('deve retornar uma estrutura de dados igual ao computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  it('caso nenhum parametro seja passado, deve retornar um erro com a mensagem: You must provide an url', async () => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
+  });
 });
