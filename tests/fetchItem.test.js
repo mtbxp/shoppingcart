e420f9b80c8ee4fa910daa023210f82bdee1224d
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-03 19:15:44 -0300

    teste função ok e teste fetch ok

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,12 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('3 - Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('4 - Com o argumento "MLB1615760527", a função fetch é chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled;
+  })
 });
