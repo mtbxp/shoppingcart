d2f15b9ca5cc682511ee0a7d710b50a364e5589a
Juliana Alvares <alvares.juliana@hotmail.com>
2022-06-03 16:07:36 -0300

    Requisito 1 concluído - testes para a função fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,33 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Se fetchProducts é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  test('Se a uma função fetch é chamada e com o edpoint correto, ao executar a função fetchProducts com o argumento "computador"', async () => {
+    expect.assertions(2);
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  test('Se a função fetchProducts retorna um objeto igual ao objeto computadorSearch, quando passado o argumento "computador"', async () => {
+    expect.assertions(1);
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+
+  test('Se a função fetchProducts retorna um erro com a mensagem "You must provide an url", quando chamada sem agumento', async () => {
+    expect.assertions(1);
+    const responseNoParam = await fetchProducts();
+    expect(responseNoParam).toEqual(new Error('You must provide an url'));
+  });
+
+  // test('Se a função fetchProducts retorna um erro com a mensagem "", quando chamada com agumento inválido', async () => {
+  //   expect.assertions(1);
+  //   const responseWrongParam = await fetchProduct('InvalidProduct');
+  //   expect(responseWrongParam).toEqual(new Error(''));
+  // });
 });
