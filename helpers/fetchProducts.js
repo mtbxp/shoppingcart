d1e6f03ae82085f189f5f3b2a99f988d7548f9c0
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-06-03 17:15:18 -0300

    requisito 2 corrigido

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,7 +4,7 @@ const fetchProducts = async (computador) => {
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
     const response = await fetch(url);
     const data = await response.json();
-    return data.results;
+    return data;
   } catch (error) {
     return error;
   }
