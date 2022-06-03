b55fd9d8e42dc2f4637017f890b8995429e40210
Dimy Leo <leonardoprimoc@hotmail.com>
2022-06-03 15:28:16 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,19 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('É uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('Com o argumento computador, retorna o fetch', () => {
+    expect(fetchProducts(computador)).toBe('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  })
+  it('Com o argumento computador, utiliza endpoint', () => {
+    expect(fetchProducts(computador)).toBeCalledWith(computador);
+  })
+  it('Testa se o retorno é igual computadorSearch', () => {
+    expect(fetchProducts(computador)).toEqual(computadorSearch);
+  })
+  it('Sem argumento retorna erro', () => {
+    expect(fetchProducts('')).toThrow('You must provide an url');
+  })
 });
