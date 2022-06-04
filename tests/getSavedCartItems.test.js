60252067309539d434740a01231eb156942a3f90
Carla Uyemura <carla.uyemura@gmail.com>
2022-06-04 15:49:51 -0300

    Requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,13 +5,12 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  it("Verifica se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado", async () => {
-    await saveCartItems('<ol><li>Item</li></ol>')
+  it("Verifica se, ao executar getSavedCartItems, o método localStorage.getItem é chamado", async () => {
+    await getSavedCartItems()
     expect(localStorage.setItem).toHaveBeenCalled();
   });
-  it("e, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems", async () => {
-    const param = '<ol><li>Item</li></ol>';
-    await saveCartItems(param)
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems' , param);
+  it(" Verifica se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o 'cartItems' como parâmetro", async () => {
+    await getSavedCartItems()
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems');
   });
 });
