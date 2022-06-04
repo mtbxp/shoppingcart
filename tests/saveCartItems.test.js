10ff850fe861bb68eaf52e04d4e928cf43cd0b4e
Dimy Leo <leonardoprimoc@hotmail.com>
2022-06-04 20:22:29 -0300

    Requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se é uma função', () => {
+    expect(typeof saveCartItems).toBe('function');
+  })
+  it('Testa se com o argumento <ol><li>Item</li></ol> é chamado o localStorage.setItem', async () => {
+    const resp = await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+  it('Testa se com o argumento <ol><li>Item</li></ol> é chamado dois parametros', async () => {
+    const resp = await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
 });
