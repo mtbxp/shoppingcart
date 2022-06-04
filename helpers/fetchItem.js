b0451a805cf70a0268b03f45b2f34cf47310d27d
Marcelo Ataíde <msa.trybe@gmail.com>
2022-06-04 10:00:18 -0300

    Requisito 3.2 - Testa se a função `fetch` foi invocada

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
-  // seu código aqui
+const BASE_URL = 'https://api.mercadolibre.com';
+const createUrlToGetProductItem = (productId) => `${BASE_URL}/items/${productId}`;
+
+const fetchItem = async (productId) => {
+  const url = createUrlToGetProductItem(productId);
+  await fetch(url);
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -7,4 +7,11 @@ describe('2 - Teste a função fetchItem', () => {
     expect.assertions(1);
     expect(typeof fetchItem).toBe('function');
   });
+
+  it('Deveria ser capaz de invocar a função fecth.', async () => {
+    expect.assertions(1);
+    const productid = 'MLB1615760527';
+    await fetchItem(productid);
+    expect(fetch).toHaveBeenCalled();
+  });
 });
