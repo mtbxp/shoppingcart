bbd1158f9cb57cc444ed2a3b3a1ca8d865d82a36
zephyr664 <dbrgamei5@gmail.com>
2022-06-03 23:23:37 -0300

    Testes do fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  const result = await fetch(url);
+  const data = await result.json();
+
+  console.log(data);
 };
+
+fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,19 @@
 require('../mocks/fetchSimulator');
+const { type } = require('mocha/lib/utils');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
+const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Se fetchProducts é um função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  });
+  it('Se a função foi chamada', () => {
+    fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  it('Teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint correto', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenLastCalledWith(endPoint);
+  });
 });
