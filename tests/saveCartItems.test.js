f88aa59688fd809990b5104feb30ac46c61df520
renanbigm <renanbigm@gmail.com>
2022-06-04 00:42:15 -0300

    requisito 6 completo

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,9 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  it('When saveCartItems is run with <ol><li>Item</li></ol> as argument, localStorage.setItem is called', async () => {
-    await saveCartItems(<ol><li>Item</li></ol>);
+  it('saveCartItems is a function', () => {
+    expect(typeof saveCartItems).toBe('function');
+  });
+  it('When saveCartItems is run with <ol><li>Item</li></ol> as argument, localStorage.setItem is called', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toBeCalled();
   });
-  
+  it('When saveCartItems is run with <ol><li>Item</li></ol> as argument, localStorage.setItem is called with the right two arguments', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
