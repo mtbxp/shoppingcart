009a9912863d1bbca7432c38860a61c13dd03578
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-06-03 22:31:46 -0300

    requisito 1 feito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,28 @@
-const fetchProducts = () => {
-  // seu código aquiii
+const fetchProducts = async (param) => {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+  const resultado = await fetch(endpoint)
+   .then((response) => response.json())
+   .then((obj) => obj)
+   .catch((error) => error);
+
+  return resultado;
 };
+
+const creatList = async () => {
+  const obj = await fetchProducts('computador');
+  const section = document.getElementsByClassName('items');
+  return obj.results.map((item) => {
+    const { id, title, thumbnail } = item
+    const obj2 = { sku: id, name: title, image: thumbnail };
+    return section.appendChild(createProductItemElement(obj2))
+  });
+};
+creatList()
 
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
   };
 }
+
+window.onload = () => creatList();
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,25 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Teste se fetch foi chamada com o endpoint correto', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('Teste se o retorno é igual ao objeto computadorSearch', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+  it('Teste se retorna um erro ao achamar a função sem parâmetro', async () => {
+    const msg = 'You must provide an url';
+    const response = await fetchProducts();
+    expect(response).toEqual(new Error(msg));
+  });
 });
