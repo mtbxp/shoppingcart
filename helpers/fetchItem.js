9980bdc60c7afea148115ec9cf9e1203fc81db22
Marcela <arqmrcl@gmail.com>
2022-06-04 20:45:54 -0300

    req 03

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = (MLB1615760527) => {
+    const url = `https://api.mercadolibre.com/items/${MLB1615760527}`;
+  
+  const result = fetch(url)
+  .then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => error);
+  return result;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -12,6 +12,15 @@ describe('2 - Teste a função fetchItem', () => {
     expect(fetch).toHaveBeenCalled();
   });
 
+  it('Verifique se ao receber ao receber a função fetchItem ao receber o argumento computador o fetch utiliza o endpoint pedido', async () => {
+    const endpoint = `https://api.mercadolibre.com/items/MLB1615760527`;
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('Verifique se ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem "You must provide an url"', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'))
+  });
 
 
 });
