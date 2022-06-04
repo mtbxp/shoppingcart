9872f589019332621a34c8adb9180784daa8c66b
Raynara Santiago <raysantiagobz@gmail.com>
2022-06-04 17:33:04 -0300

    adiciona requisito 3, cobertura de teste 100% da função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = () => {
+const fetchItem = async (MLB1615760527) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/items/${MLB1615760527}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
