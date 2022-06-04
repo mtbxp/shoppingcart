5269ca8b4892a726c872bb51157681c462324a1b
Dimy Leo <leonardoprimoc@hotmail.com>
2022-06-04 20:28:57 -0300

    Requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se é uma função', () => {
+    expect(typeof getSavedCartItems).toBe('function');
+  })
+  it('Testa se é chamado o localStorage.getItem', async () => {
+    const resp = await getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
+  it('Testa se é chamado o parametros cartItems ao chamar a função', async () => {
+    const resp = await getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  })
 });
