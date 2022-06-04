7d8cbec5308ade922e01a0926ae94eb3899e1cbf
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-06-04 17:27:26 -0300

    Requizito-01 concluido. VAMOS COM TUDO :rocket: :convenience_store:

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,15 @@
-const fetchProducts = () => {
+const fetchProducts = async (endpoint) => {
   // seu código aqui
+  try {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
+fetchProducts('endpoint').then((data) => console.log(data));
 
 if (typeof module !== 'undefined') {
   module.exports = {
