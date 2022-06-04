8c4e5f5dfbfd8e2afd769116fadb9b6d5d1f8337
Silverio Moraes <silverstone191013@gmail.com>
2022-06-03 21:30:05 -0300

    requesito-1 :rocket:

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,25 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  });
+  it('a função fetchProducts chamada com o argumento "computador",ver se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(url);
+  });
+  it('se o retorno da função fetchProducts é igual ao objeto computadorSearch', async () =>{
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+  it('com o argumento vazio, retorna um erro com a mensagem esperada', async () =>{
+    const response = await getMagicCard();
+    expect(response).toEqual(new Error('You must provide an url'));
+  });
+  // fail('Teste vazio');
 });
