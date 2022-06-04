11f3d7faa4f275c061ba377c730123d3c9c1012a
cassio <cassiospcjunior@gmail.com>
2022-06-04 08:42:12 -0300

    Requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,21 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Ao  executar saveCartItems(<ol><li>Item</li></ol>) localStorage.setItem é chamado', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Ao  executar saveCartItems(<ol><li>Item</li></ol>) localStorage.setItem é chamado com os parametros corrtos', async () => {
+    const response = await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartltems', '<ol><li>Item</li></ol>');
+  });
+
+  it('Teste se saveCartItems é uma função', async () => {
+    expect(typeof saveCartItems).toBe('function');
+  });
+
+  it('Teste se localStorageSimulator é uma função', async () => {
+    expect(typeof localStorageSimulator).toBe('function');
+  });
 });
