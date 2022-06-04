3be120149a35f76fefb8c497e050dcfae8301f1a
Breno Nascimento <satsukebare23@gmail.com>
2022-06-04 08:46:33 -0300

    adicionando testa na função getSavedCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,4 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
+  it('verifica se localStorage.getItem é chamdo', async() => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('verifica se localStorage.getItem é chamado com o parametro `cartItems`', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  })
 });
