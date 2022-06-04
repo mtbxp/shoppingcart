e8f2e792f472b6e3b7c9e3553254c82bb2976545
celsorodrigo <celsorodrigoma@gmail.com>
2022-06-04 15:07:28 -0300

    Criados testes da função getSavedCartItems.

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,21 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should call localStorage.setItem when getSavedCartItems() is called', async () => {
+    await getSavedCartItems();
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+
+  it('Should call localStorage.setItem with \'cartItems\' as parameter when getSavedCartItems is called', async () => {
+    await getSavedCartItems('');
+    expect(localStorage.setItem).toBeCalledWith('cartItems');
+  })
+
+  it('Should return new Error (\'Occorreu um erro. :c\') when getSavedCartItems is called with an invalid parameter.', async () => {
+    expect(await getSavedCartItems('INVALID')).toEqual(new Error('Occorreu um erro. :c'));
+  });
+
+  it('Should return function when typeof getSavedCartItems is called.', () => {
+    expect(typeof getSavedCartItems).toBe('function');
+  });
 });
