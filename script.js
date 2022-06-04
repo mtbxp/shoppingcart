eca7eb3368791e94a2de8851af95518718cdf454
Marcela <arqmrcl@gmail.com>
2022-06-04 20:54:50 -0300

    correções lint req 03

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 const createProduct = () => {
   fetchProducts('computador')
   .then((element) => element.results
-  .forEach(e => itemProduct.appendChild(createProductItemElement(e))));
-}
+  .forEach((e) => itemProduct.appendChild(createProductItemElement(e))));
+};
 createProduct();
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
