615b849518eab5d2838753deb30dba72c65dd79f
João Mattedi <joaommattedi@gmail.com>
2022-06-03 21:13:15 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const items = document.querySelector('.items');
 const cartItems = document.querySelector('.cart__items');
+const clearCartButton = document.querySelector('.empty-cart');
+let totalPrice = 0;
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -30,18 +32,43 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
+  const element = event.target.innerText;
+  const price = element.slice(element.indexOf('$') + 1);
+  totalPrice -= price;
+  const priceDiv = document.querySelector('.total-price');
   event.target.remove();
+  priceDiv.innerText = Math.round(totalPrice * 100) / 100;
+  return priceDiv;
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 };
 
+const createPriceElement = () => {
+  const div = document.createElement('div');
+  div.className = 'total-price';
+
+  clearCartButton.parentNode.insertBefore(div, clearCartButton);
+};
+
+const totalPriceCalculator = (price) => {
+  const priceDiv = document.querySelector('.total-price');
+  totalPrice += price;
+  priceDiv.innerText = Math.round(totalPrice * 100) / 100;
+  return priceDiv;
+};
+
+const saveCartPrice = (price) => {
+  localStorage.setItem('totalCartPrice', price);
+};
+
 window.onload = () => {
+  createPriceElement();
   cartItems.innerHTML = getSavedCartItems();
   fetchProducts('computador')
     .then((data) => data.results.forEach((element) =>
@@ -50,8 +77,10 @@ window.onload = () => {
     if (e.target.classList.contains('item__add')) {
       fetchItem(getSkuFromProductItem(e.target.parentNode))
         .then((item) => {
+          totalPriceCalculator(item.price);
           cartItems.appendChild(createCartItemElement(item));
           saveCartItems(cartItems.innerHTML);
+          saveCartPrice(totalPrice);
         });
     }
     if (e.target.classList.contains('cart__item')) return cartItemClickListener(e);
