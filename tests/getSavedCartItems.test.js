a4956e3c788953fd4f26767930666fd5ca0b28d2
renanbigm <renanbigm@gmail.com>
2022-06-04 00:46:08 -0300

    requisito 7 complet

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('getSavadCartItems is a function', () => {
+    expect(typeof getSavedCartItems).toBe('function');
+  });
+  it('When getSavadCartItems is run with <ol><li>Item</li></ol> as argument, localStorage.setItem is called', () => {
+    getSavedCartItems();
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('When getSavadCartItems is run with <ol><li>Item</li></ol> as argument, localStorage.setItem is called with the right argument', () => {
+    getSavedCartItems();
+    expect(localStorage.setItem).toBeCalledWith('cartItems');
+  });
 });
