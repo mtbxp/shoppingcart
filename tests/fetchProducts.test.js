1daea9f53831709c6680d3a2b4697e30755571d3
FabioSC <fabioscarvalh@yahoo.com.br>
2022-06-04 22:37:40 -0300

    Request 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,11 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchProducts é uma função', async () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Testa se ao chamar fetchProducts o fetch foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
