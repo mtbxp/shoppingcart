7378e94f86be9833b7e764b10919b70edba62595
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-06-04 22:41:27 -0300

    TDD requizito. VAMOS COM TUDO :rocket: :convenience_store:

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -6,4 +6,19 @@ localStorageSimulator('setItem');
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
   //fail('Teste vazio');
+  test('Testar se ao executar o saveCartItems <ol><li>Item</li></ol> localStorage.setItem é chamado', async () => {
+   await saveCartItems("<ol><li>Item</li></ol>");
+    expect(localStorage.setItem).toHaveBeenCalled()
+  });
+  test('Testar se ao executar saveCartItems <ol><li>Item</li></ol> localStorage.setItem é chamado com o paramento correto', async () => {
+    await saveCartItems("<ol><li>Item</li></ol>")
+    expect(localStorage.setItem).toHaveBeenCalledWith("cartItems","<ol><li>Item</li></ol>");
+  });
+  test('Testar se o fetchProducts retorna um error', async () => {
+    try {
+      await saveCartItems();
+    }catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'))
+    }
+  });
 });
