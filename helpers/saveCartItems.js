cd4bb36cb44719fdd1320486a486f06f00d71bd8
Marcela <arqmrcl@gmail.com>
2022-06-05 14:02:31 -0300

    correcoes lint

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,9 +1,6 @@
-const cartItems = document.querySelector('.cart__items');
-
 const saveCartItems = (savelist) => {
   localStorage.setItem('cartItems', savelist);
 };
-
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
