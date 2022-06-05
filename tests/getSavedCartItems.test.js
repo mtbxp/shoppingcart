2f79ae8810aeee05ae7fd1f4c26e279c5901c720
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-06-04 23:11:45 -0300

    TDD requizitos. VAMOS COM TUDO :rocket: :convenience_store:

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,14 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  //fail('Teste vazio');
+  test('Testar se getSavedCartItems localStorage.getItem é chamado', async () => {
+  await getSavedCartItems()
+  expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  test('Testar se getSavedCartItems localStorage.getItem é chamado', async () => {
+    await getSavedCartItems()
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems')
+  });
 });
+
