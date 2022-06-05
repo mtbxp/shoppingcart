2628317047be8cf296d6dc9a8c53fa75fa742a8b
Manuela França <manuelaalves26@gmail.com>
2022-06-05 20:51:54 -0300

    Implementei a função fetchProducts.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,9 @@
+/* eslint-disable sonarjs/no-extra-arguments */
 const fetchProducts = () => {
-  // seu código aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
 };
+
+fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,28 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('deve testar se fetchProducts é uma função', async () => {
+    expect(typeof await fetchProducts).toEqual('function');
+  });
+
+  test('deve passar "computador" como argumento e testar se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('com o argumento "computador", a função fetch deve ser chamada com o endpoint correto', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  test('deve testar se o retorno da função com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+
+  test('deve testar se, ao chamar a função sem argumento retorna um erro com a mensagem: "You must provide an url', async () => {
+    const response = await fetchProducts();
+    expect(response).toEqual(new Error('You must provide an url'));
+  });
 });
