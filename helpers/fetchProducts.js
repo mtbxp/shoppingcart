3a93b02018d655398ddad54b3965a3cb19658864
Alexandre Bernardes <abernardes11192@gmail.com>
2022-06-06 15:04:30 -0300

    Função fetchProducts feita

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (produto) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
+  try {
+    const fProduto = await fetch(url);
+    const data = await fProduto.json();
+    return data;
+  } catch (error) {
+    console.log('Produto não encontrado');
+  }
 };
 
 if (typeof module !== 'undefined') {
