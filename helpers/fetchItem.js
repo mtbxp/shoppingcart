f7c30bb3e05bc885da161f9936495a6bc2f90d15
Danilo Martins <dansm86@outlook.com>
2022-06-06 17:06:11 -0300

    Req.4-Implementado a função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
+const fetchItem = async (itemId) => {
   // seu código aqui
+  if (itemId === undefined) {
+    throw new Error('You must provide an url');
+  }
+  const url = `https://api.mercadolibre.com/items/${itemId}`;
+  const result = await fetch(url);
+  const data = await result.json();
+  console.log(data);
+  return data;
 };
 
 if (typeof module !== 'undefined') {
