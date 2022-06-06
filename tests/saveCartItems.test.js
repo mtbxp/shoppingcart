765a2da2967ebceb5cccd608d6856f9b3a290292
Marcos Schettino <schettinomarcosb@gmail.com>
2022-06-06 11:01:43 -0300

    requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,12 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('should call \'localStorage.setItem\' method when called with \'<ol><li>Item</li></ol>\' as parameter', () => {
+    saveCartItems(<ol><li>Item</li></ol>);
+    expect(localStorage.setItem).toHaveBeenCalled()
+  });
+  test('\'localStorage.setItem\' should have the expected arguments \'cartItems\' and \'<ol><li>Item</li></ol>\'', () => {
+    saveCartItems(<ol><li>Item</li></ol>);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
 });
