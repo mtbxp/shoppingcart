93a3cb1e8f512b42f0fef909fadf98b0faa6c34a
Marcelo Ataíde <msa.trybe@gmail.com>
2022-06-06 10:02:04 -0300

    Requisito 7.2 - Testa se `localStorage.getItem` e chamado com o parâmetro correto em `getSavedCartItems`

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,10 +4,17 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  it('Deveria chamar localStorage.getItem quando getSavedCartItems é chamado', () => {
+  it('Deveria chamar localStorage.getItem em algum momento', () => {
     expect.assertions(1);
     const keyName = 'catItems';
     getSavedCartItems(keyName);
     expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Deveria chamar localStorage.getItem com o parâmetro "keyName"', () => {
+    expect.assertions(1);
+    const keyName = 'cartItems';
+    getSavedCartItems(keyName);
+    expect(localStorage.getItem).toHaveBeenCalledWith(keyName);
   });
 });
