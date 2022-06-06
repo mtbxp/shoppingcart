4ea4f67361530caddefef4e957662512701fd0d5
Marcelo Ataíde <msa.trybe@gmail.com>
2022-06-06 09:54:50 -0300

    Requisito 7.1 - Testa se `localStorage.getItem` e chamado em algum momento em `getSavedCartItems`

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,10 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deveria chamar localStorage.getItem quando getSavedCartItems é chamado', () => {
+    expect.assertions(1);
+    const keyName = 'catItems';
+    getSavedCartItems(keyName);
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
 });
