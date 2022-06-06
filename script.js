2f65ec3afe19d68295d0612802b459d3f9b48b72
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-06-06 17:47:02 -0300

    REQ 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const importItems = document.querySelector('.items');
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,7 +14,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -38,4 +40,13 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+function productsItems() {
+  fetchProducts('computador').then((items) => {
+  items.results.forEach((itemChild) => {
+  importItems.appendChild(createProductItemElement(itemChild));
+});
+});
+}
+window.onload = () => { 
+  productsItems(); 
+};
