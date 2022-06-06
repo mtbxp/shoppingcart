002badd1959ce21eb03f180b65a6a09503a3dcb7
JorgeKz <jorgekzbra@gmail.com>
2022-06-06 20:20:49 -0300

    Implementando requisitos 04 e 05

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* const { fetchProducts } = require('./helpers/fetchProducts');
- */
+const items = document.querySelector('.items');
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,10 +26,10 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+  event.target.remove();
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
@@ -40,18 +40,43 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   return li;
 };
 
-const listOfProducts = () => {
-  const items = document.querySelector('.items');
-  fetchProducts('computador').then((data) => {
-    const productsArray = data.results;
-    productsArray.forEach((element) => {
-      const item = createProductItemElement(element);
-      items.appendChild(item);
-    });
-    console.log(productsArray);
+const addAndRemoveProductToCart = () => {
+  fetchProducts('computador')
+    .then((data) => data.results.forEach((element) =>
+      items.appendChild(createProductItemElement(element))));
+  document.body.addEventListener('click', (e) => {
+    if (e.target.classList.contains('item__add')) {
+      const itemID = e.target.parentNode.firstChild.innerText;
+      fetchItem(itemID).then((item) =>
+        document.querySelector('.cart__items').appendChild(createCartItemElement(item)));
+        saveCartItems(document.querySelector('.cart_items'));
+    }
+    if (e.target.classList.contains('cart_item')) {
+      cartItemClickListener(e);
+    }
   });
 };
 
+/* const addAndRemoveToCart = (data) => {
+  const itemCart = createCartItemElement(data);
+  itemCart.addEventListener('click', (event) => {
+    event.target.remove();
+  });
+  cartItems.appendChild(itemCart);
+};
+
+const addEventToItems = () => {
+  items.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item_add')) {
+      const productId = getSkuFromProductItem(event.target.parentElement);
+      fetchItem(productId).then((product) => {
+        cartItems.appendChild(createCartItemElement(product));
+        saveCartItems(cartItems.innerHTML);
+    });
+  }
+});
+}; */
+
 window.onload = () => {
-    listOfProducts();
- };
+  addAndRemoveProductToCart();
+};
