522a21075fec1e1c1bd0f94c5cadd5ff7ae19961
Marcos Schettino <schettinomarcosb@gmail.com>
2022-06-06 17:16:41 -0300

    requisitos 6 e 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,5 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
+  it('should call \'localStorage.getItem\' method', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled()
+  });
+  it('should call \'localStorage.getItem\' with \'cartItems\' as parameter', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems')
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,11 +5,11 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   it('should call \'localStorage.setItem\' method when called with \'<ol><li>Item</li></ol>\' as parameter', () => {
-    saveCartItems(<ol><li>Item</li></ol>);
+    saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled()
   });
   test('\'localStorage.setItem\' should have the expected arguments \'cartItems\' and \'<ol><li>Item</li></ol>\'', () => {
-    saveCartItems(<ol><li>Item</li></ol>);
+    saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
   })
 });
