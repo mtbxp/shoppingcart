65260b5ef3e9db019ebfb44f9b8fc07deb1a15e4
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-06 19:39:25 -0300

    localStorage getItem

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,8 @@
 const saveCartItems = (idItem) => {
-   localStorage.setItem('cartItems', idItem);
+  localStorage.setItem('cartItems', idItem);
+
+  const itemLocalStorage = JSON.stringify(idItem);
+  localStorage.setItem('cartItems', itemLocalStorage);
  };
 
 if (typeof module !== 'undefined') {
