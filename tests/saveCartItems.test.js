bec969fc17c5408bc2e8d439cb19b18e4699d72b
lucaseduardolema <lucaseduardolema@gmail.com>
2022-06-06 15:34:32 -0300

    add req 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem', () => {
+    const arg = '<ol><li>Item</li></ol>';
+    saveCartItems(arg);
+    expect(localStorage.setItem).toBeCalled()
+  });
+
+  test('saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, "cartItems" o valor passado como argumento para saveCartItems', () => {
+    const arg = '<ol><li>Item</li></ol>';
+    saveCartItems(arg);
+    expect(localStorage.setItem).toBeCalledWith('cartItems', arg);
+  });
 });
