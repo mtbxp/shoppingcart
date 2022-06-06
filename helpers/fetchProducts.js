1c0a2ea0d7325adf63b7891e3424c5fa08024f44
Vitormonteiro28 <vitormonteirotomaz7@gmail.com>
2022-06-06 11:14:49 -0300

    requisito 1 and 2 ok

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,11 +1,8 @@
-
-
 const fetchProducts = async (peca) => {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${peca}`
-  const response = await fetch(url)
-  return response.json()
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${peca}`;
+  const response = await fetch(url);
+  return response.json();
 };
-
 
 if (typeof module !== 'undefined') {
   module.exports = {
