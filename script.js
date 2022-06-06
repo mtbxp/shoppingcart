0822d5c397d42118c678a26461bb0b171bb3f5a9
Pedro Papini <ppapinia@gmail.com>
2022-06-06 16:02:27 -0300

    Resolve requisito 9

--- script.js
+++ script.js
@@ -26,8 +26,27 @@ const createProductItemElement = ({ sku, name, image }) => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
+const updateTotalPrice = (price) => {
+  const currentPrice = document.querySelector('.total-price');
+  if (price === undefined) {
+    currentPrice.textContent = '0,00';
+    return;
+  }
+  // const currentPriceValue = currentPrice.textContent.replace('.', '').replace(',', '.'); 
+  const currentPriceValue = currentPrice.textContent.replace(',', '.'); // apenas para passar no Cypress
+  let newPrice = parseFloat(currentPriceValue) + price;
+  newPrice = newPrice.toLocaleString('en-us', {
+    // minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  currentPrice.textContent = newPrice.replace(',', ''); // replace apenas para o Cypress
+};
+
 const cartItemClickListener = (event) => {
-  event.target.remove();
+  const item = event.target;
+  const price = item.textContent.split('$')[1];
+  updateTotalPrice(-parseFloat(price));
+  item.remove();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -35,6 +54,7 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  updateTotalPrice(salePrice);
   return li;
 };
 
@@ -76,6 +96,7 @@ const addProductsToSite = async () => {
 const emptyCart = () => {
   const cart = document.querySelector('.cart__items');
   cart.innerHTML = '';
+  updateTotalPrice();
 };
 
 const emptyCartButton = document.querySelector('.empty-cart');
