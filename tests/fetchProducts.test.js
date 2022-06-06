ed87c930d7bce0d7d7d34a1b2bda56854cb2c6d8
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-06 17:01:12 -0300

    Requisito 1 testes implementados, aguardando os códigos da função para execução e finalização

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,22 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio ');
+  it('Verificar se `fetchProducts` é uma funçaõ', async () => {
+    await expect(typeof fetchProducts).toBe('function');
+  });
+  it('Verificar se a função `fetchProducts` recebendo o parâmetro `computador`, testa se `fetch`foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes();
+  });
+  it('Verificar se a função `fetchProducts` utiliza o endpoint esperado', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledWith(url)
+  });
+  it('Verificar se o retorno da função `fetchProducts` recebendo o parâmetro `computador` tem sua estrutura igual ao objeto `computadorSearch`', async () => {
+    expect(typeof await fetchProducts('computador')).toBe(typeof computadorSearch);
+  });
+  it('Verificar se ao chamar a função `fetchProducts` sem argumentos, retorna um erro com uma mensagem', async () => {
+    await expect(fetchProducts()).toEqual(new Error ('You must provide an url'));
+  });
 });
