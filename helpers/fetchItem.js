b10097c5189e91dbd7f485930cf6a62164afe5b7
JorgeKz <jorgekzbra@gmail.com>
2022-06-06 14:57:35 -0300

    Implementando requisitos 02 e 03

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemID) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${itemID}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,8 +1,27 @@
 require('../mocks/fetchSimulator');
+const { type, assign } = require('mocha/lib/utils');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  })
+  it('Executa a função fetchItem com o argumento do item \'MLB1615760527\' e testa se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Verifica se ao chamar a função fetchItem com o argumento \'MLB1615760527\', a função fetch utiliza o endpoint \'https://api.mercadolibre.com/items/MLB1615760527\'', async () => {
+    const endpoint ="https://api.mercadolibre.com/items/MLB1615760527";
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalledWith(endpoint);
+  })
+  it('Verifica se o retorno da função fetchItem com o argumento do item \'MLB1615760527\' é uma estrutura de dados igual ao objeto \'item\'', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  })
+  it('Verifica se ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: \'You must provide an url\'', async () => {
+    const result = await fetchItem();
+    expect(result).toEqual(new Error('You must provide an url'));
+  })
 });
