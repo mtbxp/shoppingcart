712b8ebb5d9b22e40aa40894f3dd57695685bf7f
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-06-06 18:19:04 -0300

    primeiro teste do requisito 1 implementado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,6 +2,8 @@ const fetchProducts = () => {
   // seu código aqui
 };
 
+console.log(fetchProducts);
+
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,9 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
+  const fetchProductsFunction = fetchProducts;
+  it('Verifica se fetchProdutcs é uma função', () => {
+    expect(typeof (fetchProductsFunction)).toBe('function');
+  })
   fail('Teste vazio');
 });
