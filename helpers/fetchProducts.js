7bca7b7b2cca2b3387cfa3429fcf797bd045f8fa
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-06 17:38:35 -0300

    Requisito 2 em desenvolvimento

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,17 @@
-const fetchProducts = () => {
+const fetchProducts = async (param) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 };
+
+console.log(fetchProducts());
 
 if (typeof module !== 'undefined') {
   module.exports = {
