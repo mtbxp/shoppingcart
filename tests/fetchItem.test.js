66fe9c8ef664f1e1e9169bdc758114a93583dd80
Miyuki Ishii <miyukisabrina@gmail.com>
2022-06-06 09:53:13 -0300

    Requisito 3 - Desenvolvendo testes de no mínimo 50% da cobertura total da função fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -2,8 +2,29 @@ require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
-describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  
-  fail('Teste vazio');
+describe('2 - Test fetchItem function', () => {
+  it('Should be a function', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Should call the fetch function, given the argument `MLB1615760527`', async () => {
+    await fetchItem('MLB1615760527');
+    expect.assertions(1);
+    expect(fetch).toBeCalled();
+  });
+  it('Should call fetch function with expected endpoint, with argument `MLB1615760527`', async () => {
+    const url = "https://api.mercadolibre.com/items/MLB1615760527";
+    await fetchItem('MLB1615760527');
+    expect.assertions(1);
+    expect(fetch).toBeCalledWith(url);
+  });
+  it('Should return the `item` object given the argument `MLB1615760527`', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect.assertions(1);
+    expect(result).toEqual(item);
+  });
+  it('Should return a error message given no argument', async () => {
+    const result = await fetchItem();
+    expect.assertions(1);
+    expect(result).toEqual(new Error('You must provide an url'));
+  });
 });
