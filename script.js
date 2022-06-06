0019a213aad4971bbf4c262b4f02c85db17bacb6
Kauan <kauan.s.leite@hotmail.com>
2022-06-06 13:28:18 -0300

    teste do requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bunda = document.getElementsByClassName('items')[0]
+const sectionItems = document.getElementsByClassName('items')[0];
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -14,17 +14,15 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-
-const teste = async (index) => {
-  const data = await fetchProducts('computadores')
+const listagem = async (index) => {
+  const data = await fetchProducts('computador');
 
   const sku = data.results[index].id;
   const name = data.results[index].title;
   const image = data.results[index].thumbnail;
 
-  return {sku, name, image};
-}
-
+  return { sku, name, image };
+};
 
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
@@ -38,23 +36,23 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
+// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+// const cartItemClickListener = (event) => {
+//   // coloque seu código aqui
+// };
 
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
-};
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
+// const createCartItemElement = ({ sku, name, salePrice }) => {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// };
 
 window.onload = async () => {
-  for(let index = 0; index < 48; index += 1){
-    bunda.appendChild(createProductItemElement(await teste(index)))
+  for (let index = 0; index < 12; index += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    sectionItems.appendChild(createProductItemElement(await listagem(index)));
   }
-}
+};
