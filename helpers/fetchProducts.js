1cff4a2403c9d0e575fa21abefb6c4ee5fe95967
Larissa <larissamf25@gmail.com>
2022-06-06 16:26:20 -0300

    testando requisitos 1 a 4 sem o require

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,6 @@
 // Project <Pixels art> from <Larissa Menezes> done in 22.06.03 for the Trybe course, ninth week. It has been used as reference the notes from the class an external links indicated along the code line
 
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const getURL = (product) => `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
 
