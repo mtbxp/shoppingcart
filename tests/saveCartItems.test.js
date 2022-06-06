03bca476e28ed104c44f2c1e768028b705b89f88
DaniloLO53 <olin_ad@hotmail.com>
2022-06-06 15:02:12 -0300

    Testando o requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    localStorage.setItem('', '');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  test('', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    localStorage.setItem('cartItems', '<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
