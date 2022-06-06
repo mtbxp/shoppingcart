9d8cee02ac1b1b03556c54c8dfb74b5f335a345c
leonardo-schneider2 <leocoelho256@gmail.com>
2022-06-06 19:05:06 -0300

    exercicio 1 entregue

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
+const fetchProducts = async (computer) => {
   // seu código aqui
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computer}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,23 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('testa caso fetchProducts é uma função: ', async () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+  test('testa caso fetch foi chamado, e caso foi chamado com url adequado: ', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  test('testa caso o retorno da função seja igual computerSearch: ', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+  test('ao chamar a função fetchProducts sem argumento retorna um erro com a mensagem "You must provide an url"', async () => {
+    const response = await fetchProducts();
+    expect(response).toEqual(new Error('You must provide an url'))
+  });
 });
