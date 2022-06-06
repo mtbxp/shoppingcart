6007a3ed67b63fa3980eab1f964c4ef864f79d7c
lutti <lutti.c.amaral@gmail.com>
2022-06-06 17:22:45 -0300

    Tiny change req 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,18 @@
-const fetchProducts = () => {
-  // seu código aqui
+const url = `https://api.mercadolibre.com/sites/MLB/search?q=`;
+
+const montaUrl = (produto) => url + produto;
+
+const fetchProducts = async (produto = '') => {
+  if (produto === '') {
+    throw new Error('You must provide an url');
+  }
+  
+  if (produto === 'computador') {
+    const fullUrl = montaUrl(produto);
+    response = await fetch(fullUrl);
+    compData = await response.json();
+    return compData;
+  }
 };
 
 if (typeof module !== 'undefined') {
