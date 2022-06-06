2d82b9b64f1216835f52a4ea05512669fdd66991
Matheus <Maths.Aki@gmail.com>
2022-06-06 17:45:12 -0300

    começando a 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
-const fetchProducts = () => {
+const fetchProducts = async () => {
   // seu código aqui
+  const query = 'computador';
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const response = await fetch(endpoint);
+  const final = await response.json();
+  console.log(final);
 };
+
+// createProductItemElement();
 
 if (typeof module !== 'undefined') {
   module.exports = {
