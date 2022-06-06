b93633062a84db4465ae2f30e507d206eeffb5e8
DiogoSales <diogosales.ads@gmail.com>
2022-06-06 17:51:23 -0300

    requisito 1 ok

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,12 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deve retorna uma function', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Deve retorna uma function', () => {
+    expect(fetchProducts('computador')).toHaveBeenCalled();
+  });
+  // fail('Teste vazio');
 });
