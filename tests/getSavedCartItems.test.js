9825524a8944297515f69653b4f06c89dfafdbd4
Artur Vidor <vidorartur@gmail.com>
2022-06-06 22:22:53 -0300

    100% dos testes

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,12 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado;', ()=> {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled()
+  })
+  test('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o \'cartItems\' como parâmetro.', () => {
+    getSavedCartItems();
+     expect(localStorage.getItem).toBeCalledWith('cartItems')
+   });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -9,7 +9,7 @@ describe('3 - Teste a função saveCartItems', () => {
   saveCartItems('<ol><li>Item</li></ol>')
    expect(localStorage.setItem).toBeCalled()
  });
- test('este se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
+ test('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
   saveCartItems('<ol><li>Item</li></ol>')
    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>')
  });
