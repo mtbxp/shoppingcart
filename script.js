877896417e41134053adb9d2b863d7740af04835
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-06-07 18:56:22 -0300

    requisito 4 OK

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const sectionItems = document.querySelector('.items');
 
+const ol = document.querySelector('.cart__items');
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,10 +9,35 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
+const cartItemClickListener = (event) => {
+  
+};
+
+const createCartItemElement = ({ id: sku, title: name, thumbnail, price: salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.appendChild(createProductImageElement(thumbnail));
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+function addItemCart(event) {
+  if (event.target.classList.contains('item__add')) {
+  const getSku = getSkuFromProductItem(event.target.parentElement);
+  fetchItem(getSku).then((objeto) => {
+   ol.appendChild(createCartItemElement(objeto));
+  });
+  }
+}
+
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  e.addEventListener('click', addItemCart);
   return e;
 };
 
@@ -24,20 +51,6 @@ const createProductItemElement = ({ id, title, thumbnail }) => {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-};
-
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
-};
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 };
 
 fetchProducts('computador')// retorna a API tratada com o JSON() vulgo 'response'.
