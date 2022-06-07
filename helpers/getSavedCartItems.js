706b91daf46b7c96f113a83f27410ccb2b9bddbe
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-07 12:19:25 -0300

    reformulando a função

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,7 +1,6 @@
-const getSavedCartItems = (cartItems) => {
-  JSON.parse(localStorage.getItem(cartItems));
+const getSavedCartItems = () => {
+  localStorage.getItem('cartItems');
 };
-
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
 }
