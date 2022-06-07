99c89c0ab282e696079d88a70b1f1df6db42304c
rubimeryka <erykarubim13@gmail.com>
2022-06-07 11:33:45 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,31 @@
 const fetchProducts = () => {
-  // seu código aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+
+  const products = fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => error.toString());
+
+    return products;
 };
+
+//const setCoins = async () => {
+//  const coins = await fetchCoins();
+
+ // const coinsList = document.getElementById('coins-list');
+
+  //coins.forEach((coin) => {
+    //const newLi = document.createElement('li');
+    //const usdPrice = Number(coin.priceUsd);
+
+    //newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
+
+    //coinsList.appendChild(newLi);
+  //});
+//}
+
+window.onload = () => setCoins();
+
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,14 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchProducts é uma função', async () => {
+    const isFunction = await fetchProducts();
+    expect(isFunction).toBe('function');
+  });
+
+  it('Testa se, ao chamar a função fetchProducts sem argumento, retorna uma mensagem de erro', async () => {
+    const failRequest = await fetchProducts();
+    expect(failRequest).toEqual(new Error('You must provide un url'));
+  });
 });
+
