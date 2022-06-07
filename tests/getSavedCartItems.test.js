20c4813c0c99ff4d562177a6b262cc332e804383
Ricardo <ricardo_md_25@hotmail.com>
2022-06-07 18:41:23 -0300

    exercise 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, getSavedCartItems ao chamar localStorage', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+  it('Teste se, ao executar getSavedCartItems, ao chamar localStorage com "cartItems"',async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  });
 });
