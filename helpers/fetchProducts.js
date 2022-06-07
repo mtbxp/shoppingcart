25dffc52de6d9bec1e3a0e73dd12109ac36b051f
bigJhow88 <jhow.and@gmail.com>
2022-06-07 19:05:43 -0300

    Rquisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async ($QUERY) => {
+  if ($QUERY === undefined) {
+    return 'You must provide an url';
+  } if (typeof $QUERY === 'string') {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
 };
 
 if (typeof module !== 'undefined') {
