0bcf47a00cf4c1bd571f30b63fd83fe316cbe64c
HeitorFernandes <heitorsoaresfer@gmail.com>
2022-06-06 23:05:15 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,6 +22,15 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
+};
+
+const renderProducts = async () => {
+  const items = document.querySelector('.items');
+  const data = await fetchProducts('computador');
+  data.results.forEach((product) => {
+    const item = createProductItemElement(product);
+    items.appendChild(item);
+  });
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
@@ -38,4 +47,6 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => {
+  renderProducts();
+};
