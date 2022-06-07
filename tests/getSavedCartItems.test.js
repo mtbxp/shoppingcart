bc314128c9996966fc40e00e3571dcb9986c03a5
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-06-07 11:18:03 -0300

    Req 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,16 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  /* fail('Teste vazio'); */
+
+  test('Deve chamar o método localStorage.setItem', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  test('Deve chamar o getSavedCartItems localStorage.getItem é chamado passando parâmetro', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
+  
 });
