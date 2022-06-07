9de3ac1994eb25c11fb09da9728f643de8c993b6
rubimeryka <erykarubim13@gmail.com>
2022-06-07 11:39:50 -0300

    consertando lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -9,23 +9,22 @@ const fetchProducts = () => {
     return products;
 };
 
-//const setCoins = async () => {
+//  const setCoins = async () => {
 //  const coins = await fetchCoins();
 
  // const coinsList = document.getElementById('coins-list');
 
-  //coins.forEach((coin) => {
-    //const newLi = document.createElement('li');
-    //const usdPrice = Number(coin.priceUsd);
+  //  coins.forEach((coin) => {
+    //  const newLi = document.createElement('li');
+    //  const usdPrice = Number(coin.priceUsd);
 
-    //newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
+    //  newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
 
-    //coinsList.appendChild(newLi);
-  //});
-//}
+    //  coinsList.appendChild(newLi);
+  //  });
+//  }
 
-window.onload = () => setCoins();
-
+window.onload = () => fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
