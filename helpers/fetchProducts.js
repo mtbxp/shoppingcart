28d9f4447ed644b64adfb78e71b0aab27a304720
Danilo de Barros Ribeiro <nilo22@gmail.com>
2022-06-06 23:01:00 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,8 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (argument) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${argument}`;
+  const responseData = fetch(url).then((response) => response.json()).then((data) => data);
+
+  return responseData;
 };
 
 if (typeof module !== 'undefined') {
