3da60421346d3cdb11a65d036060e60a8e586952
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-07 11:51:04 -0300

    localStorage ok

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,10 +1,7 @@
 const saveCartItems = (idItem) => {
   localStorage.setItem('cartItems', idItem);
-
-  const itemLocalStorage = JSON.stringify(idItem);
-  localStorage.setItem('cartItems', itemLocalStorage);
- };
+};
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
-}
+} 
