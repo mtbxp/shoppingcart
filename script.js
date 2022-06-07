2bc4dcf75bb321a6b9908c90f396af0f09a6bf97
Henos <henosvinicius13@gmail.com>
2022-06-07 15:38:51 -0300

    Resolução dos requisitos 09 e 10

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const priceParagraph = document.querySelector('p');
+
+if (localStorage.getItem('prices') === null) {
+  priceParagraph.innerText = 0;
+} else {
+  const getPrice = localStorage.getItem('prices');
+  priceParagraph.innerText = getPrice;
+}
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,18 +21,38 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// Obs.: O código para arredondar valores abaixo foi baseado em um post do StackOverflow
+function roundFloats(value) {
+  return Math.round(value * 100) / 100;
+}
+
+function sumCartItems({ price }) {
+  let actualPrice = JSON.parse(localStorage.getItem('prices'));
+  actualPrice += price;
+  localStorage.setItem('prices', roundFloats(actualPrice));
+  return roundFloats(actualPrice);
+}
+
+function pricesSub(price) {
+  let actualPrice = localStorage.getItem('prices');
+  actualPrice -= price;
+  localStorage.setItem('prices', roundFloats(actualPrice));
+  return roundFloats(actualPrice);
+}
+
 const createCartItemElement = ({ id, title, price }, listFather) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   li.addEventListener('click', (event) => {
+    priceParagraph.innerText = pricesSub(price);
     event.target.remove();
     saveCartItems(listFather.innerHTML);
   });
   return li;
 };
 
-const createProductItemElement = ({ id, title, thumbnail }, fatherEle, listFather) => {
+const createProductItemElement = ({ id, title, thumbnail }, listFather) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -35,6 +64,7 @@ const createProductItemElement = ({ id, title, thumbnail }, fatherEle, listFathe
     const data = await fetchItem(id);
     listFather.appendChild(createCartItemElement(data, listFather));
     saveCartItems(listFather.innerHTML);
+    priceParagraph.innerText = sumCartItems(data);
   });
   
   section.appendChild(cartButton);
@@ -46,22 +76,34 @@ const addItems = async (name, fatherEle, listFather) => {
   const data = await fetchProducts(name);
   for (let index = 0; index < 50; index += 1) {
     const info = data[index];
-    fatherEle.appendChild(createProductItemElement(info, fatherEle, listFather));
+    fatherEle.appendChild(createProductItemElement(info, listFather));
   }
 };
 
 function addListenerToSavedItems(newList, list) {
   for (let index = 0; index < newList.length; index += 1) {
-    newList[index].addEventListener('click', (event) => {
+    const getList = newList[index];
+    getList.addEventListener('click', async (event) => {
       event.target.remove();
       localStorage.setItem('cartItems', JSON.stringify(list.innerHTML));
+      const id = getList.innerText.slice(5, 18);
+      const data = await fetchItem(id);
+      priceParagraph.innerText = pricesSub(data.price);
     });
   }
 }
 
 window.onload = () => {
+  const getClearButton = document.querySelector('.empty-cart');
   const itemsSection = document.querySelector('.items');
   const list = document.querySelector('ol');
+
+  getClearButton.addEventListener('click', () => {
+    list.innerHTML = '';
+    localStorage.removeItem('cartItems');
+    localStorage.removeItem('prices');
+    priceParagraph.innerText = 0;
+  });
 
   addItems('computador', itemsSection, list);
 
