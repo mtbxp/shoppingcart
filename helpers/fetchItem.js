47856c06c927072d6c2d3bc3c37b78982578fe54
Artur Vidor <vidorartur@gmail.com>
2022-06-06 21:34:56 -0300

    quarto e quinto exercicio

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,9 +1,9 @@
-const fetchItem = async (id) => {
+const fetchItem = async (itemId) => {
   // seu código aqui
-  if (id === undefined) {
+  if (itemId === undefined) {
     return 'You must provide an url';
   }
-  const url = `https://api.mercadolibre.com/items/${id}`;
+  const url = `https://api.mercadolibre.com/items/${itemId}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
--- script.js
+++ script.js
@@ -5,37 +5,13 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
+
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 };
-
-const createProductItemElement = ({
-  sku,
-  name,
-  image,
-}) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  console.log(section);
-  const item = document.querySelector('#item');
-  item.appendChild(section);
-  return section;
-};
-
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
-};
-
 const createCartItemElement = ({
   sku,
   name,
@@ -45,16 +21,65 @@ const createCartItemElement = ({
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  const cartItem = document.querySelector('.cart__items');
+  cartItem.appendChild(li);
+  li.addEventListener('click', (event) => {
+    if (document.querySelector('.selected')) {
+      document.querySelector('.selected').classList.remove('selected');
+    }
+    event.target.remove();
+  });
   return li;
 };
+
+const prepareCartList = async (itemId) => {
+  const data = await fetchItem(itemId);
+  const salePrice = data.price;
+  const sku = data.id;
+  const name = data.title;
+  const result = {
+    sku,
+    name,
+    salePrice,
+  };
+  createCartItemElement(result);
+};
+const teste = (event) => {
+ const data = event.target.parentNode.firstChild.innerText;
+ prepareCartList(data);
+};
+
+const createProductItemElement = ({
+  sku,
+  name,
+  image,
+}) => {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const item = document.querySelector('#item');
+  const addItem = document.querySelectorAll('.item__add');
+  item.appendChild(section);
+for (let index = 0; index < addItem.length; index += 1) {
+  addItem[index].addEventListener('click', teste);
+}
+};
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const cartItemClickListener = (event) => {};
+
+
 
 const prepareSite = async () => {
   const data = await fetchProducts('computador');
   const dataLength = data.results;
   const index = Math.round(Math.random() * dataLength.length);
-  const sku = data.results[index].id;
-  const name = data.results[index].title;
-  const image = data.results[index].thumbnail;
+  const sku = await data.results[index].id;
+  const name = await data.results[index].title;
+  const image = await data.results[index].thumbnail;
   const result = {
     sku,
     name,
@@ -63,14 +88,13 @@ const prepareSite = async () => {
   createProductItemElement(result);
 };
 
-const siteItens = async () => {
-  const data = await fetchProducts('computador');
-  const dataLength = data.results;
-  for (let index = 0; index < dataLength.length; index += 1) {
+async function siteItens() {
+   await fetchProducts('computador');
+  for (let index = 0; index < 50; index += 1) {
     prepareSite();
   }
-};
+}
 
 window.onload = () => {
-  siteItens(); 
+  siteItens();
 };
