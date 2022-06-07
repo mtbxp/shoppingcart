74fafb96ba905793a1ce2c3301cf0d8e759956ee
HeitorFernandes <heitorsoaresfer@gmail.com>
2022-06-07 00:41:14 -0300

    Requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,14 @@
-const fetchItem = () => {
-  //
+const fetchItem = async (param) => {
+  try {
+  const url = `https://api.mercadolibre.com/items/${param}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+  } catch (error) {
+    return error;
+  }
 };
-
+fetchItem();
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,21 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('checks if fetchItem is a function', () => {
+    expect(typeof fetchItem).toBe('function');
+  })
+  it('checks if fetch is being called inside fetchItem', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('checks if fetch inside fetchItem uses the specified endpoint', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  })
+  it('returns a data structure similar to "item" when argument for fetchItem is "MLB1615760527', async () => {
+    expect(await fetchItem('MLB1615760527')).toStrictEqual(item);
+  })
+  it('returns error when fetchItem is called without an argument', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  })
 });
