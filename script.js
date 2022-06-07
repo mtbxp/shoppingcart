ade314435780dd963a00c45a5b54eded44ed4e4e
leovcampos <leonardoviniciuscampos@gmail.com>
2022-06-07 15:19:21 -0300

    CSS

--- script.js
+++ script.js
@@ -55,7 +55,6 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
   return li;
 };
 
