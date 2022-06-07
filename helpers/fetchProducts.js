f0b5681187f300c5747f2c3e4007d776eaee9a26
zephyr664 <dbrgamei5@gmail.com>
2022-06-07 12:22:40 -0300

    Requisito 3 e Requisito 4

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,8 +2,6 @@ const fetchProducts = async (product) => {
   try {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
   const result = await response.json();
-  // const data = await result.json();
-  // console.log(data);
   return result;
   } catch (error) {
     return error;
