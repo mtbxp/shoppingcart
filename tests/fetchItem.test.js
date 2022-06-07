c87728230647bd59483414c8d5326382c74add3d
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-06-07 14:58:17 -0300

    Exercicio 3 - daqui a pouco decubro se está ok

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,21 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('verifica sw fetchItem é uma função', () => {
+    expect(fetchItem).toBeInstanceOf(Object);
+  });
+  it('verifica se ao chamar a função fetchItens com item "MLB1615760527" o fetch é chamado', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('verifica se a função fetchItem usa o endpoint correto', async () => {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(await fetchItem('MLB1615760527')).toHaveBeenCalledWith(url);
+  });
+  it('verifica se o retorno de fecthItens é igual ao objeto item', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('verifica se ao chamar a função sem argumento retorna a mensagem: "You must provide an url"', () => {
+    expect(fetchItem()).toEqual(new Error('You must provide an url'));
+  });
 });
