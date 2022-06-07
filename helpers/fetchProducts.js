e22dcb084c73b108e5117b799faf0e35f5a94a5c
Anderson Jaco <andersoncoelho2010@hotmail.com>
2022-06-07 18:54:38 -0300

    tentando requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = (query) => {
+const fetchProducts = async (query) => {
   const urlApi = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  try {
+    const itemApi = await fetch(urlApi);
+    const data = await itemApi.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+
 };
 
 if (typeof module !== 'undefined') {
