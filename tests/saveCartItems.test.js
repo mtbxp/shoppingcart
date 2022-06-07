918dc73223602b7394bd6893d544a8be3649eba3
Ricardo <ricardo_md_25@hotmail.com>
2022-06-07 18:16:07 -0300

    exercise 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,13 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, a função saveCartItems ao chamar o método localStorage.setItem', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('Teste se, ao executar saveCartItems, o método localStorage.setItem é chamado dois parametros', async () => {
+    const obj = '<ol><li>Item</li></ol>';
+    await saveCartItems(obj);
+    expect(localStorage.setItem).toBeCalledWith('cartItems', obj);
+  });
 });
