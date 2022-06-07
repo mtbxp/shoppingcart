2b7181d8587a9a36b527300bd42bdb9f048d6f09
adyluna <adylunaleite@gmail.com>
2022-06-07 19:05:59 -0300

    gogo

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -3,8 +3,8 @@ const fetchProducts = async (product) => {
 
   const result = await fetch(URL)
     .then((response) => response.json())
-    .then((data) => Object.values(data.results))
-    .catch((error) => `${error}`);
+    .then((data) => data.results) //once was Object.values(data.results)
+    .catch((error) => error);
 
   return result;
 };
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,14 +4,25 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   it('Should be a function', async () => {
-    const result = fetchProducts;
-    
-    expect(typeof result).toBe('function');
+        
+    expect(typeof fetchProducts).toBe('function');
   });
-  // it('Should be a function', async () => {
-  //   const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  it('Should call fetch 1 time', async () => {
+    const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
     
-  //   fetchProducts('computador');
-  //   expect(fetch).toHaveBeenCalledWith(URL);
-  // });
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledTimes(1);
+  });
+  it('Should be called with URL', async () => {
+    const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(URL);
+  });
+  it('Should be equal do computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch.results);
+  });
+  it('Should return "You must provide an url" if called without arguments', async () => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
+  });
 });
