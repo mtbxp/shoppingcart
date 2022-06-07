92e0888707b72379eac543cf27d7cdea388ad30e
Manuela França <manuelaalves26@gmail.com>
2022-06-07 13:18:03 -0300

    Criei a requisição para a função fetchItem.

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  try {
+  const endpoint = `https://api.mercadolibre.com/items/${id}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+} catch (error) {
+  return error;
+}
 };
 
 if (typeof module !== 'undefined') {
