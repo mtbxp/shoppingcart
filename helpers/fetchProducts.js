c2ba2b6f56c6ae12910e802ef674cc8b29528a0b
adyluna <adylunaleite@gmail.com>
2022-06-07 00:49:59 -0300

    Complicated at first but I know I can do this

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,16 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetch = require('node-fetch');
+const { thumbnail } = require('../mocks/item');
+const { results } = require('../mocks/search');
+
+const fetchProducts = async (callback) => {
+  const URL = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+
+  const result = await fetch(URL)
+    .then((response) => response.json())
+    .then((data) => Object.values(data.results)
+      .forEach(({ id, title, thumbnail }) => callback({id, title, thumbnail})))
+    .catch((error) => `${error}`)
+  return result;
 };
 
 if (typeof module !== 'undefined') {
