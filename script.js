47985c1d87cbb7f6ab99f678d6fc43d93501c3b6
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-06-07 14:44:31 -0300

    arrumando o script

--- script.js
+++ script.js
@@ -15,6 +15,44 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
+//   const section = document.createElement('section');
+//   section.className = 'item';
+
+//   section.appendChild(createCustomElement('span', 'item__sku', sku));
+//   section.appendChild(createCustomElement('span', 'item__title', name));
+//   section.appendChild(createProductImageElement(image));
+//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+//   section.addEventListener('click', async (element) => { 
+//   const itemID = element.target.parentNode.firstChild.innerText;
+//   const item = await fetchItem(itemID);
+//   cartItems.appendChild(createCartItemElement(item)); 
+//   });
+
+//   return section;
+// };
+
+// const showComputers = async () => {
+//   const products = await fetchProducts('computador');
+//   products.results.forEach((endpoint) => {
+//   const append = createProductItemElement(endpoint);
+//   sectionItem.appendChild(append);
+// });
+// };
+
+const cartItemClickListener = (event) => {
+  event.target.remove();
+};
+
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
 const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -39,18 +77,6 @@ const showComputers = async () => {
   const append = createProductItemElement(endpoint);
   sectionItem.appendChild(append);
 });
-};
-
-const cartItemClickListener = (event) => {
-  event.target.remove();
-};
-
-const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 };
 
 const clearCart = () => {
