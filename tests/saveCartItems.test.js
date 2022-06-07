9f9278d11c9da50dc4e6fe3bec6052a7c9744929
leonardo-schneider2 <leocoelho256@gmail.com>
2022-06-07 00:04:55 -0300

    Acho que saiu o 6 também, mas tá mal feito ainda

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,28 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se saveCartItems é uma função: ', () => {
+    expect.assertions(1);
+
+    expect(typeof saveCartItems).toBe('function');
+    
+  });
+  test('se quando chamamos a função saveCartItems com parametro <ol><li>Item</li></ol> o localStorage.setItem é chamado: ', async () => {
+    expect.assertions(1);
+
+    let a;
+    a.innerHTML = '<ol><li>item</li></ol>';
+    await saveCartItems(a);
+
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  test('se quando chamamos a função saveCartItems com parametro <ol><li>Item</li></ol> o localStorage.setItem é chamado com dois parâmetros: ', async () => {
+    expect.assertions(1);
+
+    let a;
+    a.innerHTML = '<ol><li>item</li></ol>';
+    await saveCartItems(a);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', a);
+  });
 });
