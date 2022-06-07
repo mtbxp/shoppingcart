fa378ae14e0b0a07f16ae2012393b4953eb5e21c
adyluna <adylunaleite@gmail.com>
2022-06-06 21:30:24 -0300

    1st ok

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,15 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should be a function', async () => {
+    const result = fetchProducts;
+    
+    expect(typeof result).toBe('function');
+  });
+  // it('Should be a function', async () => {
+  //   const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    
+  //   fetchProducts('computador');
+  //   expect(fetch).toHaveBeenCalledWith(URL);
+  // });
 });
