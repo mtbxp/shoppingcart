c4f68bc390c79b8e32e318d1a7bab8cb60dc2d9f
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-06-07 11:09:38 -0300

    Req 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,21 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  /* fail('Teste vazio'); */
+
+  test('Deve chamar o método localStorage.setItem', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  test('Deve chamar o método localStorage.setItem com os dois metodos corretos', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
+
+  test('Deve retornar um erro com a mensagem: "You must provide an url"', async () => {
+    const error = new Error('You must provide an url');
+    await expect(saveCartItems()).resolves.toThrow(error);
+  });
+
 });
