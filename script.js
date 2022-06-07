27e87d3831fb3b7d421b11ba3eaa7510f22ed07a
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-06-07 15:52:35 -0300

    9 feito

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const sectionItems = document.querySelector('.items');
 const olCartItems = document.querySelector('.cart__items');
-const somandoItems = document.createElement('div');
+const totalPrice = document.querySelector('.total-price');
 const cart = document.querySelector('.cart');
+const btnClear = document.querySelector('.empty-cart');
 
+// codigo prepronto
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 };
-
+// codigo prepronto
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 };
-
+// codigo prepronto
 const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -28,16 +30,30 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 
   return section;
 };
-
+// codigo prepronto
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-// FEITO POR MIM
+// FEITO POR MIm
+function auxilSub(arg) {
+  const valorPraSubTratar = Number(arg.split('$')[1]);
+  return valorPraSubTratar;
+}
+
+let precoFinal = 0;
+
+function somaPreco(arg) {
+  const numAprox = Math.round((precoFinal + arg) * 100) / 100;
+  precoFinal = numAprox;
+  totalPrice.innerText = numAprox;
+  return totalPrice.innerText;
+}
+
 const cartItemClickListener = (event) => {
-  const itemCart = document.querySelector('.cart__item');
-  if (event.target.classList.contains('cart__item')) {
-    itemCart.remove();
-    saveCartItems(olCartItems.innerHTML);
-  }
+  const valorPraSubTratar = Number(event.target.innerText.split('$')[1]);
+  somaPreco(-valorPraSubTratar);
+  event.target.remove();
+  saveCartItems(olCartItems.innerHTML);
+  // }
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
@@ -53,39 +69,22 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
 // Resgatando itens dela FetchAPI
 const getProducts = () => {
   fetchProducts('computador').then((products) =>
-  products.results.forEach((product) =>
-  sectionItems.appendChild(createProductItemElement(product))));
+    products.results.forEach((product) =>
+      sectionItems.appendChild(createProductItemElement(product))));
 };
 //
-let precoFinal = 0;
-
-function somaPreco() {
-  somandoItems.innerText = precoFinal;
-  somandoItems.className = 'total-price';
-  cart.appendChild(somandoItems);
-}
-
-function somaAuxiliar(arg) {
-  precoFinal += parseFloat(arg);
-  somaPreco();
-  return precoFinal;
-}
-
-function subtracaoAuxiliar() {
-  
-}
 
 // Evento de click que adiciona ao carrinho
 sectionItems.addEventListener('click', (event) => {
   if (event.target.classList.contains('item__add')) {
-  const sectionItem = event.target.parentElement;
-  const idFound = getSkuFromProductItem(sectionItem);
-  fetchItem(idFound).then((produto) => {
-  somaAuxiliar(produto.price);
-  olCartItems.appendChild(createCartItemElement(produto));
-  saveCartItems(olCartItems.innerHTML);
-});
-}
+    const sectionItem = event.target.parentElement;
+    const idFound = getSkuFromProductItem(sectionItem);
+    fetchItem(idFound).then((produto) => {
+      somaPreco(produto.price);
+      olCartItems.appendChild(createCartItemElement(produto));
+      saveCartItems(olCartItems.innerHTML);
+    });
+  }
 });
 // STORAGES
 // função para adicionar items do carrinho ao localStorage
