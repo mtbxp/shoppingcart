19ccd8cd611f0f06969e66e270c530fdab8c0ba7
DaniloLO53 <olin_ad@hotmail.com>
2022-06-07 09:48:04 -0300

    testando nvamente

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,12 +1,11 @@
 const getSavedCartItems = () => 
   // seu código aqui
   // console.log((localStorage.getItem('cartItems'), 8));
-   JSON.parse(localStorage.getItem('cartItems'))
+   JSON.parse(localStorage.getItem('cartItems'));
   // if (localStorage.getItem('cartItems') !== undefined) {
   //   return JSON.parse(localStorage.getItem('cartItems'));
   // }
   // return null;
-;
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
