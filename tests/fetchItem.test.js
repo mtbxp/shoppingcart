e5ed6e3e58bedc1dd065f8e1376b021106e63e22
adyluna <adylunaleite@gmail.com>
2022-06-07 19:34:16 -0300

    Requisito 3 ok

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,21 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should be a function', async () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Should fetch with URL', async () => {
+    const URL = 'https://api.mercadolibre.com/items/MLB1615760527';
+    fetchItem('MLB1615760527');
+
+    expect(fetch).toBeCalledWith(URL);
+  });
+  it('Should equals item', async () => {
+    expect(fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('Should error message "You must provide an url"', async () => {
+    const expectedError = new Error('You must provide an url');
+    
+    expect(fetchItem()).toEqual(expectedError);
+  });
 });
