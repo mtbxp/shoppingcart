fd8164a5d3b22f1e004e4592d5b1a390196f82e1
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-06-07 18:23:45 -0300

    5. removi o item do carinho

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  console.log(event);
+  const select = document.querySelector('.cart__items');
+  select.removeChild(event.path[0]);
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
