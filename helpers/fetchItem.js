455e46210a74d760d3f29960f16709555e3cf628
Vitormonteiro28 <vitormonteirotomaz7@gmail.com>
2022-06-07 11:14:44 -0300

    requisito 3 ok

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,7 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  const url = `https://api.mercadolibre.com/items/${id}`;
+  const response = await fetch(url);
+  return response.json();
 };
 
 if (typeof module !== 'undefined') {
