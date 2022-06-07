485993a58645b662e89d11d8ab13544f55d3daac
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-07 18:09:14 -0300

    refatorando e deixando o projeto 100%

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 };
 
