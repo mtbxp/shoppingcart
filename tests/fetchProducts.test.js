7e6191cb14f279bbc68bda2b71b82c2d5f92331e
Melania P Chagas <melaniapchagas@gmail.com>
2022-06-07 13:22:04 -0300

    Requisito 1: implementando teste 2/5 na função 'fetchProducts'

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,9 @@ describe('1 - Teste a função fetchProducts', () => {
   test ('Verifica se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function')
   })
+  test('Verifica se: ao ser chamada com o argumento "computador", retorna a resposta da url : ', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+    // Use .toHaveBeenCalledWith para garantir que uma função de simulação (mock, em inglês) foi chamada com argumentos específicos. link: https://jestjs.io/pt-BR/docs/expect#tohavebeencalled
+  })
 });
