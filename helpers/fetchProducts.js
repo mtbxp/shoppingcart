2b2f56900e537e17e15003759d9e34c200cde68d
rubimeryka <erykarubim13@gmail.com>
2022-06-08 15:59:17 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,33 +1,13 @@
-const fetchProducts = () => {
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
-
-  const products = fetch(url)
-    .then((response) => response.json())
-    .then((data) => data.data)
-    .catch((error) => error.toString());
-
-    return products;
+const fetchProducts = async () => {
+  const QUERY = 'computador';
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  await fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 };
-
-//  const setCoins = async () => {
-//  const coins = await fetchCoins();
-
- // const coinsList = document.getElementById('coins-list');
-
-  //  coins.forEach((coin) => {
-    //  const newLi = document.createElement('li');
-    //  const usdPrice = Number(coin.priceUsd);
-
-    //  newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
-
-    //  coinsList.appendChild(newLi);
-  //  });
-//  }
-
-window.onload = () => fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
-    fetchProducts,
+  fetchProducts,
   };
-}
+  } 
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,13 +5,20 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
   it('Testa se fetchProducts é uma função', async () => {
-    const isFunction = await fetchProducts();
-    expect(isFunction).toBe('function');
+    expect(typeof fetchProducts).toBe('function'); 
   });
-
-  it('Testa se, ao chamar a função fetchProducts sem argumento, retorna uma mensagem de erro', async () => {
-    const failRequest = await fetchProducts();
-    expect(failRequest).toEqual(new Error('You must provide un url'));
+  it('Testa se a função é chamada com o argumento computador', async () => {
+    const computador = await fetchProducts('computador');//executa a função 
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('testa endpoint', async () => {
+    await fetchProducts('computador');
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('testa se retorna erro', async () => {
+    const retorno = await fetchProducts();
+    expect(retorno).toEqual(new Error ('You must provide a url'));
   });
 });
 
