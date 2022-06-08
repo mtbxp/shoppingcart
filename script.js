6142f0d293ca010743a0f8f4d65481d95e99d4f3
Maikel <maikelmysilvestre@gmail.com>
2022-06-08 15:52:22 -0300

    1-7

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ const createProductItemElement = ({ sku, name, image }) => {
 
 // const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = () => {
-  this.innerHTML = '';
+const cartItemClickListener = (event) => {
+  event.target.remove();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('saveCartItems é uma função?', () => {
+    expect(typeof getSavedCartItems).toBe('function');
+  });
+  test('ao executar getSavedCartItems, o método localStorage.getItem é chamado;', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  test('ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro.', async () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith(cartItems);
+  });
 });
