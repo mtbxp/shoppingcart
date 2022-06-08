852aa4beef5d417e1b15deaf1d1a70aa46446c54
adyluna <adylunaleite@gmail.com>
2022-06-07 23:21:15 -0300

    Requisitos 5 e 6 ok

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myCart = document.querySelector('.cart__items');
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,22 +14,10 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-};
-
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+  myCart.removeChild(event.target);
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -36,6 +26,29 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+};
+
+const toCart = async (event) => {
+  const id = getSkuFromProductItem(event.target.parentElement);
+  const response = await fetchItem(id);
+
+  myCart.appendChild(createCartItemElement({
+    sku: response.id, 
+    name: response.title, 
+    salePrice: response.price }));
+};
+
+const createProductItemElement = ({ sku, name, image }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const button = section.querySelector('button');
+  button.addEventListener('click', toCart);
+  return section;
 };
 
 const itensSection = document.querySelector('.items');
@@ -47,19 +60,6 @@ const toLoad = async () => {
   .appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail })));
 };
 
-const myCart = document.querySelector('.cart__items');
-
-const toCart = async () => {
-  const toCartItem = await fetchItem('MLB1341706310');
-  const newArr = Object.values(toCartItem);
-
-  myCart.appendChild(createCartItemElement({
-    sku: toCartItem.id, 
-    name: toCartItem.title, 
-    salePrice: toCartItem.price }));
-};
-
 window.onload = async () => {
   await toLoad();
-  await toCart();
 };
