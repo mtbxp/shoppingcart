078ef579ccd3bfd6d5bc4ae3f2d203f4269ccd79
Rafael Vitor <rafaelvitor@itaoca.net>
2022-06-08 02:44:06 -0300

    Requisitos 1 e 2 (TDD)

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async ($QUERY) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  } catch (erro) {
+    return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
