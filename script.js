30fccafc5b276870c8ceb7830c1fc295fa1a263e
HugoWSL <hugodesousa.16@gmail.com>
2022-06-08 12:16:57 -0300

    implementação da função que adiciona e remove itens do carrinho

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* const { fetchProducts } = require("./helpers/fetchProducts"); */
 const products = document.querySelector('.items');
+const cart = document.querySelector('.cart__items');
+// Cria lista de produtos 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -25,11 +27,10 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+  event.target.remove();
 };
+// Adiciona Elemntodos ao carrinho elemetos no carrinho 
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
@@ -39,29 +40,40 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-const getItem = async () => {
-  const test = await fetchItem('MLB1615760527');
+const getItem = async (parm) => {
+  const test = await fetchItem(parm);
   const cartItem = {
     sku: test.id,
     name: test.title,
     salePrice: test.price,
   };
-  createCartItemElement(cartItem);
+  cart.appendChild(createCartItemElement(cartItem));
 };
-getItem();
+
+const addTocart = (event) => {
+  getItem(event.target.parentNode.firstChild.innerHTML);
+};
 
 const productsList = async () => {
   const listItens = await fetchProducts('computador');
   listItens.forEach((product) => {
-    const item = {
+    const itens = {
       sku: product.id,
       name: product.title,
       image: product.thumbnail,
     };
 
-products.appendChild(createProductItemElement(item));
+    products.appendChild(createProductItemElement(itens));
+  });
+  const clickProduct = document.querySelectorAll('.item__add');
+  clickProduct.forEach((button) => {
+    button.addEventListener('click', addTocart);
   });
 };
+
+// ainda n
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
 window.onload = async () => {
   productsList();
 };
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -10,17 +10,17 @@ describe('1 - Teste a função fetchItem', () => {
     fetchItem('MLB1615760527')
     expect(fetch).toHaveBeenCalledTimes(1)
   });
-  test('Teste se, ao chamar a função fetchItem com o argumento "computador", a função fetch utiliza o endpoint correto', () => {
+  test('Teste se, ao chamar a função fetchItem com o argumento "MLB1615760527", a função fetch utiliza o endpoint correto', () => {
     fetchItem('MLB1615760527')
 
     expect(fetch).toHaveBeenLastCalledWith("https://api.mercadolibre.com/items/MLB1615760527")
   });
-  test('Teste se o retorno da função fetchItem com o argumento "computador" é uma estrutura de dados igual ao objeto "computadorSearc"', async () => {
+  test('Teste se o retorno da função fetchItem com o argumento "MLB1615760527" é uma estrutura de dados igual ao objeto "MLB1615760527Searc"', async () => {
     const result = await fetchItem ('MLB1615760527');
 
     expect(result).toEqual(item) 
   });
-  test('Testa se fetch foi chamada', async () => {
+  test('Testa se fetchItem sem parametro retorna um erro', async () => {
     const resultErr = await fetchItem();
     expect(resultErr).toEqual(new Error('You must provide an url'))
   });
