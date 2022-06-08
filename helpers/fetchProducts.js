2c00117f150698200b6acfed62227131b0d2a06b
carolcortes <caroline.ocortes@gmail.com>
2022-06-08 19:23:43 -0300

    requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,8 +1,8 @@
 const fetchProducts = async (product) => {
   try {
     const url = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
-    const results = await url.json();
-    return results;
+    const response = await url.json();
+    return response.results;
   } catch (err) {
     throw new Error('You must provide an url');
   }
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = (sku, name, image) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,6 +22,13 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
+};
+
+const getProducts = async () => {
+  const items = document.querySelector('.items');
+  const products = await fetchProducts('computador');
+  products.forEach(({ id, title, thumbnail }) => items
+  .appendChild(createProductItemElement(id, title, thumbnail)));
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
@@ -38,4 +45,6 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => {
+  getProducts();
+ };
