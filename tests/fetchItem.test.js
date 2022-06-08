cd174f584b3fc83cae2c9511e77b579a0085f783
Matheus Bora <matheusmcb@gmail.com>
2022-06-08 19:23:22 -0300

    progreq3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,15 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+it('Teste se fetchItem é uma função', () => {
+  expect(typeof fetchItem).toBe('function');
+})
+it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada;', async () => {
+  await fetchItem("MLB1615760527");
+  expect(fetch).toHaveBeenCalled();
+})
+it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";', async () => {
+  await fetchItem("MLB1615760527");
+  expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/items/MLB1615760527");
+})
 });
