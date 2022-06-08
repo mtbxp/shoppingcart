a57a2c074328f1e5188f9d046bb25df2ae706a29
rogelins <rogelins@gmail.com>
2022-06-08 20:03:45 -0300

    'requisito 10 finalizado'

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const listProducts = document.querySelector('.items');
 const sectionChart = document.querySelector('.cart__items');
 const totalprice = document.querySelector('.total-price');
+const clearCartButton = document.querySelector('.empty-cart');
 
-const pricesChart = JSON.parse(localStorage.getItem('prices')) || [];
+const pricesCart = JSON.parse(localStorage.getItem('prices')) || [];
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -30,28 +31,24 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
   return section;
 };
 
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-// console.log(getSkuFromProductItem);
-
 const somaPrecos = (array) => array.reduce((a, b) => {
     if (array === []) {
-      console.log('teste');
       totalprice.innerText = 0;
     }
     return a + b;
   }, 0);
 
 const cartItemClickListener = (event) => { 
-  const itemChart = event.target;
-  itemChart.remove();
+  const itemCart = event.target;
+  itemCart.remove();
   saveCartItems(sectionChart.innerHTML);
-  const pricesPerItem = Number(itemChart.innerText.split('$')[1]);
-  const attPrice = pricesChart.indexOf(pricesPerItem);
-  pricesChart.splice(attPrice, 1);
-  localStorage.setItem('prices', JSON.stringify(pricesChart));
+  const pricesPerItem = Number(itemCart.innerText.split('$')[1]);
+  const attPrice = pricesCart.indexOf(pricesPerItem);
+  pricesCart.splice(attPrice, 1);
+  localStorage.setItem('prices', JSON.stringify(pricesCart));
   getSavedCartItems();
-  somaPrecos(pricesChart);
-  totalprice.innerText = somaPrecos(pricesChart);
+  somaPrecos(pricesCart);
+  totalprice.innerText = somaPrecos(pricesCart);
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
@@ -59,14 +56,11 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   const pricesPerItem = Number(li.innerText.split('$')[1]);
-  pricesChart.push(pricesPerItem);
-  localStorage.setItem('prices', JSON.stringify(pricesChart));
-  totalprice.innerText = somaPrecos(pricesChart);  
+  pricesCart.push(pricesPerItem);
+  localStorage.setItem('prices', JSON.stringify(pricesCart));
+  totalprice.innerText = somaPrecos(pricesCart);
   return li;
 };
-
-const getPricesItems = () => localStorage.getItem('prices');
-console.log(getPricesItems);
 
 sectionChart.addEventListener('click', cartItemClickListener);
 
@@ -91,6 +85,15 @@ const allProducts = async () => {
 const itemsLoad = () => {
   sectionChart.innerHTML = getSavedCartItems();
 };
+
+function removeAll() {
+  localStorage.removeItem('prices');
+  localStorage.removeItem('cartItems');
+  sectionChart.innerHTML = '';
+  totalprice.innerText = '';
+}
+
+clearCartButton.addEventListener('click', removeAll);
 
 window.onload = () => {
   allProducts();
