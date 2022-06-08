dd4b7492c01f55c9e4f3d1034b393a899343850a
Rubio <paulocezarrubio@gmail.com>
2022-06-08 13:06:14 -0300

    req 6

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,16 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('verifica se a getSavedCartItens é uma função', () => {
+    expect(typeof getSavedCartItems)
+    .toBe('function')
+  });
+  it('testa se ao chamar a função getSavedCartItens,o localStorage é chamado', () => {
+    getSavedCartItems("cartItems")
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('verifica se o localStorage.getItem é chamado se o parametro for "cartItems" ', () => {
+    getSavedCartItems("cartItems")
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
