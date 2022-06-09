1635a7e9920291cf363b0a3c269cd5178f665bff
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-09 17:26:15 -0300

    Requisito 7 implementado, aguardando verificação dos testes

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,20 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verificar se `getSavedCartItems` é uma função', async () => {
+    await expect(typeof getSavedCartItems).toBe('function');
+  });
+  it('Verificar se o `localStorage.getItem` é chamado na função `getSavedCartItems`', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Verificar se o `localStorage.getItem` é chamado na função `getSavedCartItems` com parâmetros', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
+  it('Verificar a função `getSavedCartItems` retorna um erro com uma mensagem', async () => {
+    const mockError = await getSavedCartItems();
+    expect(mockError).toEqual(new Error('You must provide an url'));
+  });
 });
