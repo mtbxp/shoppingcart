f41b5ebc49479df94730fcf1f6f7c48be9ede9c9
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-09 17:17:50 -0300

    Requisito 6 implementado, aguardando verificação dos testes

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,7 +5,20 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  it('Verificar se `getSavedCartItems` é uma função', async () => {
+  it('Verificar se `saveCartItems` é uma função', async () => {
     await expect(typeof saveCartItems).toBe('function');
+  });
+  it('Verificar se o `localStorage.getItem` é chamado na função `saveCartItems`', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Verificar se o `localStorage.getItem` é chamado na função `saveCartItems` com parâmetros', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
+  it('Verificar a função `saveCartItems` retorna um erro com uma mensagem', async () => {
+    const mockError = await saveCartItems();
+    expect(mockError).toEqual(new Error('You must provide an url'));
   });
 });
