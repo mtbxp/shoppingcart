2d8ef30e853f320d72fefa73e9370a5a7a3719a9
Cássia Avellar <c.m.avellar@gmail.com>
2022-06-09 19:04:12 -0300

    Requisito 4 realizado

--- script.js
+++ script.js
@@ -22,23 +22,7 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-};
-
-async function appendFetchProducts() {
-  const result = await fetchProducts('computador');
-  const productsList = result.results.map(function (p) {
-    return {
-      sku: p.id,
-      name: p.title, 
-      image: p.thumbnail,
-    };
-  }); 
-  productsList.forEach((product) => {
-    const resultSection = createProductItemElement(product);
-    const sectionsElement = document.getElementsByClassName('items')[0];
-    sectionsElement.appendChild(resultSection);
-  }); 
- }
+}; 
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
@@ -46,34 +30,48 @@ const cartItemClickListener = (event) => { // req 5
   // coloque seu código aqui
 };
 
-const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => { // req4
+const createCartItemElement = ({ sku, name, salePrice }) => { // req4
   const li = document.createElement('li');
-  // const element = document.createElement('p');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 };
 
-// Durante a realização dos requisitos 4 e 5, obtive orientações da colega Emily Menezes
+// Durante a realização do requisito 4, recebi orientações dos colegas Willian Portela, 
+// João Mattedi, Maria Clara Reis e Emily Menezes.
 
-// function appendFetchItems(idQualquer) { // req4
-//   const resultado = createCartItemElement(idQualquer);
-//   resultado.addEventListener('click', cartItemClickListener);
-//   const ol = document.getElementsByClassName('cart__items');
-//   ol.appendChild(resultado);
-// }
-// function addProducts() { // req4
-//   const classItem = document.getElementsByClassName('items');
-//   classItem.addEventListener('click', (element) => { 
-//     if (element.target.classList.contains('item__add')) {
-//       const getId = getSkuFromProductItem(element.target.parentNode);
-//       fetchItem(getId).then((index) => { 
-//       appendFetchItems(index);
-//   // const ol1 = document.getElementsByClassName('cart__items').innerHTML; 
-//  });
-//     } 
-// });
-// }
+async function appendFetchItems(idQualquer) { // req4
+  const element1 = await fetchItem(idQualquer);
+  const { id: sku, title: name, price: salePrice } = element1;
+  const resultado = createCartItemElement({ sku, name, salePrice });
+  const ol = document.getElementsByClassName('cart__items')[0];
+  ol.appendChild(resultado);
+}
+
+function addBotao() { // req4
+  const listButton = document.querySelectorAll('.item__add');
+  listButton.forEach((botao) => botao.addEventListener('click', (event) => {
+    const eventoBotao = event.target.parentNode.firstChild.innerText;
+    appendFetchItems(eventoBotao);
+  }));
+}
+
+async function appendFetchProducts() {
+  const result = await fetchProducts('computador');
+  const productsList = result.results.map(function (p) {
+    return {
+      sku: p.id,
+      name: p.title,
+      image: p.thumbnail,
+    };
+  });
+  productsList.forEach((product) => {
+    const resultSection = createProductItemElement(product);
+    const sectionsElement = document.getElementsByClassName('items')[0];
+    sectionsElement.appendChild(resultSection);
+  });
+  addBotao();
+}
+
 window.onload = () => { appendFetchProducts(); };
-// addProducts();
