f9adb09a8591e539fb82c0b6426abe5b314cc5a3
Marcos Leite <marcosvpleite@gmail.com>
2022-06-09 18:20:52 -0300

    requisito 6 corrigido

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,6 @@
 const saveCartItems = () => {
   console.log('<ol><li>Item</li></ol>');
 };
-saveCartItems('<ol><li>Item</li></ol>');
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
