62bd1155fbaca412ae8538d506a9eb03a19e2c4a
Bambirra <ivan.bambirra@engenharia.ufjf.br>
2022-06-09 20:51:49 -0300

    volta tudo

--- script.js
+++ script.js
@@ -26,8 +26,7 @@ const createProductItemElement = ({ id, title, thumbnail }) => {
   return section;
 };
 
-// cria os produtos na pagina
-const insertProducts = async () => {
+const insertAvailableProducts = async () => {
   const itemsGroup = document.querySelector('.items');
   const data = await fetchProducts('computador');
   const allProducts = data.results;
@@ -36,25 +35,14 @@ const insertProducts = async () => {
 
 const getIDFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const calculateTotalPrice = () => {
-  const totalPriceTag = document.querySelector('.total-price');
-  const cart = document.querySelectorAll('.cart__item');
-  const cartArray = Array.from(cart);
-  const totalPrice = cartArray.reduce((total, currentProduct) => {
-    const currentPrice = currentProduct.innerText.split('$')[1];
-    return total + parseFloat(currentPrice);
-  }, 0);
-  totalPriceTag.innerHTML = Math.round(totalPrice * 100) / 100;
-};
-const cartItem = document.querySelector('.cart__items');
-
 const cartItemClickListenerDelete = (event) => {
-  cartItem.removeChild(event.target);
-  saveCartItems(cartItem.innerHTML);
+  const cart = document.querySelector('.cart__items');
+  cart.removeChild(event.target);
+  saveCartItems(cart.innerHTML);
 };
 
-const ItemClicker = () => {
-  // const cart = document.querySelectorAll('.cart__item');
+const cartItemClickListener = () => {
+  const cart = document.querySelectorAll('.cart__item');
   cart.forEach((item) => {
     item.addEventListener('click', cartItemClickListenerDelete);
   });  
@@ -64,15 +52,12 @@ const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
-  li.addEventListener('click', ItemClicker);
+  li.addEventListener('click', cartItemClickListenerDelete);
   return li;
 };
 
- const keepProducts = [];
- localStorage.setItem('products', JSON.stringify(keepProducts));
-
-const addProduct = async () => {
-  await insertProducts();
+const addProductOnCart = async () => {
+  await insertAvailableProducts();
   const itemsGroup = document.querySelector('.items');
   const selectShopBtn = itemsGroup.querySelectorAll('button');
   selectShopBtn.forEach((element) => {
@@ -80,26 +65,24 @@ const addProduct = async () => {
       const productId = getIDFromProductItem(event.target.parentElement);
       const product = await fetchItem(productId);
       const newItemOnCart = createCartItemElement(product);
-      // const cartItem = document.querySelector('.cart__items');
-      cartItem.appendChild(newItemOnCart);
+      const cart = document.querySelector('.cart__items');
+      cart.appendChild(newItemOnCart);
+      saveCartItems(cart.innerHTML);
     });
   });
 };
 
 const clearCart = () => {
-  const cartItems = document.querySelector('.cart__items');
   const emptyCartBtn = document.querySelector('.empty-cart');
   emptyCartBtn.addEventListener('click', () => {
     cartItems.innerHTML = '';
     localStorage.setItem('cartItems', '');
-    calculateTotalPrice();
   });
 };
 
 window.onload = () => { 
-  insertProducts();
-  addProduct();
-  ItemClicker();
-  calculateTotalPrice();
+  addProductOnCart();
+  getSavedCartItems();
+  cartItemClickListener();
   clearCart();
 };
