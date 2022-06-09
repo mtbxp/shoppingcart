8b98a80d2d62feea495c2bca047bfe3b9c2fce53
Ravanz <rayteon.h@gmail.com>
2022-06-08 21:41:33 -0300

    Requisito 3 completo

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,22 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui.
-  fail('Teste vazio');
+  it('deve ser uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  
+  it('deve chamar a função fetch com argumento `MLB1615760527`', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalled();
+  });
+
+  it('deve chamar a função fetchItem com o argumento `MLB1615760527`, a função fetch utiliza o endpoint `https://api.mercadolibre.com/items/MLB1615760527`', async () => {
+    await fetchItem('MLB1615760527')  
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+    it('deve retornar `You must provide an url` caso não receba argumento', async () =>{
+  ;
+  expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  });
 });
