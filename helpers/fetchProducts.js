141ba06196979dcf243e4fb97490000bb0258da5
Thais Camargo <thaisp.camargo@gmail.com>
2022-06-08 22:18:56 -0300

    Adiciona melhorias

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,12 @@
 const fetchProducts = async (param) => { 
   const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
-  const result = await fetch(API_URL);
-  const data = await result.json();
-
-  console.log(data.results);
-  return data.results;
+  try {
+    const result = await fetch(API_URL);
+    const data = await result.json();
+    return data.results;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
@@ -12,8 +14,3 @@ if (typeof module !== 'undefined') {
     fetchProducts,
   };
 }
-
-// pegar sku, name, image
-// forEach e jogar as infos na função que cria o card.
-// pegar o elemento section 'items'
-// e fazer um appendChild nele
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,26 +4,28 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  it('Verifica se fetchProducts é uma função', () => {
-    expect(typeof fetchProducts('computador')).toBe("function");
+  it('fetchProducts deve ser uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
   })
 
-  it('Verifica se fetch foi chamado quando passado "computador" como argumento da função', async () => {
+  it('Fetch deve ser chamado quando quando a função receber "computador" como parâmetro', async () => {
     await fetchProducts('computador')
     expect(fetch).toHaveBeenCalled()
   })
 
-  it('Verifica se a função fetchProducts com o parâmetro "computador" utiliza o endpoint URL', async () => {
-    const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  it('Função deve usar o endpoint correto quando receber "computador" como parâmetro', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
     await fetchProducts('computador');
-    expect(fetch).toHaveBeenCalledWith(URL)
+    expect(fetch).toHaveBeenCalledWith(endpoint)
   })
 
-  it('Verifica se o retorno da função fetchProducts com o parâmetro "computador" é igual ao objeto computadorSearch', async () => {
-    expect(await fetchProducts('computador')).toEqual(computadorSearch)
+  it('Deve ter o mesmo retorno que computadorSearch quando receber o argumento "computador"', async () => {
+    const response = await fetchProducts('computador')
+    expect(response).toEqual(computadorSearch)
   })
 
-  it('Verifica se a função fetchProducts retorna o erro "You must provide an url" quando passada sem parâmetro', async () => {
-    expect(await fetchProducts()).toThrow('You must provide an url')
+  it('Deve retornar o erro "You must provide an url" quando não receber argumento', async () => {
+    const response = await fetchProducts()
+    expect(response).toEqual(new Error('You must provide an url'))
   })
 });
