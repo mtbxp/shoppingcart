18a22d2c72d7578722075f0cda9886b4c1cce389
Matheus Queiroz <matheus_giga@hotmail.com>
2022-06-09 17:07:56 -0300

    Finalizando o requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se saveCartItems é uma função', () => {
+    expect(typeof getSavedCartItems).toBe('function');
+  });
+  test('Testa se ao executar a função o localStorage.setItem é chamado', () => {
+    getSavedCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  test('Testa se ao executar a função o localStorage.setItem é chamado com dois parâmetros', () =>{ 
+    getSavedCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith(cartItems,'<ol><li>Item</li></ol>');
+  });
 });
