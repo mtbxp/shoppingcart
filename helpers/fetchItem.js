701fa7436df5f65845c150742e9ba662f71dd0b3
Anderson Jaco <andersoncoelho2010@hotmail.com>
2022-06-09 20:19:42 -0300

    lint 4

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,14 +1,12 @@
-
 const fetchItem = async (ItemID) => {
   const urlApi = `https://api.mercadolibre.com/items/${ItemID}`;
   try {
     const itemApi = await fetch(urlApi);
-    const data =  await itemApi.json();
+    const data = await itemApi.json();
     return data;
   } catch (error) {
     return error;  
-  }
-  
+  }  
 };
 
 if (typeof module !== 'undefined') {
