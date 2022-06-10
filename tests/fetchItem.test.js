80574310cdce95c3197b2b1401f2ce5b86481f36
Guilherme Crupi <guilhermecrupi@hotmail.com>
2022-06-10 10:38:38 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,6 +4,26 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
+  // fail('Teste vazio');
+  test('testa se fetchItem é uma função', () => {
+  expect(typeof fetchItem).toBe('function');
+  });
 
-  fail('Teste vazio');
+  test('testa se a função é chamada ao receber o parametro MLB1615760527', async () => {
+  await fetchItem('MLB1615760527');
+  const fetchItemEndpoint =  'https://api.mercadolibre.com/items/MLB1615760527';
+  expect(fetch).toHaveBeenCalled();
+  expect(fetch.toHaveBeenCalledWith(fetchItemEndpoint));
+  });
+
+  test('testa se ao receber o parametro MLB1615760527 a função retorna uma estrutura de dados igual ao objeto item', async () => {
+  const retornoFetchItem = await fetchItem('MLB1615760527');
+  expect(retornoFetchItem).toEqual(item);
+  })
+
+  test('testa se ao chamar a função fetchItem sem argumento retorna um erro com a mensagem You must provide an url', async () =>{
+    const fetchItemVazio = await fetchItem();
+    expect(fetchItemVazio).toEqual(new Error('You must provide an url'));
+  })
+  
 });
