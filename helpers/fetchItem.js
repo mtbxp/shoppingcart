eee006e154f20fde4bbbd93cc88f38aef03c23ff
rubimeryka <erykarubim13@gmail.com>
2022-06-09 21:04:41 -0300

    requisito 4

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
+const fetchItem = async (ItemID) => {
   // seu código aqui
+  const urlSearchForProduct = `https://api.mercadolibre.com/items/${ItemID}`;
+  const productId = await fetch(urlSearchForProduct);
+  const data = await productId.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = (sku, name, image) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,19 +24,11 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const productsOnScreen = async () => {
-const itemSection = document.querySelector('.items');
- const listProducts = await fetchProducts('computador');
- listProducts.forEach(({ id, title, thumbnail }) => 
- itemSection.appendChild(createProductItemElement({ id, title, thumbnail }))); 
- console.log(listProducts);
-};
-
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
 };
+
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
@@ -44,6 +36,28 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+};
+
+const adicionarItens = () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => {
+    button.addEventListener('click', async (event) => {
+      const itemID = getSkuFromProductItem(event.target.parentNode);
+      const item = await fetchItem(itemID);
+      const { id: sku, title: name, price: salePrice } = item;
+      const listItem = createCartItemElement({ sku, name, salePrice });
+      document.querySelector('ol.cart__items').append(listItem);
+    });
+  }); 
+};
+
+const productsOnScreen = async () => {
+const itemSection = document.querySelector('.items');
+const listProducts = await fetchProducts('computador');
+console.log(listProducts); 
+listProducts.forEach(({ id, title, thumbnail }) => 
+ itemSection.appendChild(createProductItemElement(id, title, thumbnail))); 
+adicionarItens();
 };
 
 window.onload = () => { productsOnScreen(); };
