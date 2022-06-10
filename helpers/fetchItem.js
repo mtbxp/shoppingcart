944c335b50cb2078e1daef60608583386d9541c9
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-06-10 10:17:24 -0300

    requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (codigo) => {
+  try {
+    const promises = await fetch(`https://api.mercadolibre.com/items/${codigo}`);
+    const objeto = await promises.json();
+    return objeto;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,33 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+
+  test('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () =>{
+    fetchItem("MLB1615760527");
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";', () => {
+    fetchItem("MLB1615760527");
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/items/MLB1615760527");
+  });
+
+  test('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    const mock = await fetchItem("MLB1615760527");
+    expect(mock).toStrictEqual(item);
+  });
+  console.log(item);
+  test('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url".', async () => {
+    const mockError = await fetchItem();
+    expect(mockError).toEqual(new Error('You must provide an url'));
+  });
+  
+  
+  
+  
+  
+  
 });
