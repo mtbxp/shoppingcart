5a3b1e19def9f79ffb5f89fc36a07091c6ee15ae
Elaine <elaine.vch@gmail.com>
2022-06-09 23:54:57 -0300

    Req 2 - corrigindo lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,7 @@
 const fetchProducts = async (item) => {
   const urlfetchProducts = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
   try {
-    const response = await fetch (urlfetchProducts);
+    const response = await fetch(urlfetchProducts);
     const data = await response.json();
     return data;
   } catch (error) {
