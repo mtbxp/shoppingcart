1b95b558179a656bd5cf48d6b7c2c5c21a130dc2
mtbxp <marcellotb2@gmail.com>
2022-06-09 21:22:59 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  const response = fetch(url)
+  .then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => error);
+  return response;
 };
 
 if (typeof module !== 'undefined') {
