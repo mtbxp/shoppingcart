921e12293a81c4a2227af4ff0701303bf866a502
Elaine <elaine.vch@gmail.com>
2022-06-09 22:55:17 -0300

    Req 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui.
+const fetchProducts = async (item) => {
+  const urlfetchProducts = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  try {
+    const response = await fetch (urlfetchProducts);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
