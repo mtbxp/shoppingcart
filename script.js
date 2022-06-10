18acc5260724229eb513c7efbf382f4810328ce9
Henrique Rosa <samuel_kuik@hotmail.com>
2022-06-10 12:19:09 -0300

    Feito requisito 11

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let totalPrice = 0;
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,10 +8,7 @@ const createProductImageElement = (imageSource) => {
 
 const cartItemClickListener = (event) => {
   // coloque seu código aqui;
-  const itemCart = document.querySelector('.cart__item');
-  const cartNode = itemCart.parentNode;
-  const itemToRemove = event.target;
-  cartNode.removeChild(itemToRemove);
+  event.target.remove();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -24,12 +22,15 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
 
 const addItemToCart = async (event) => {
   const OlInCart = document.getElementsByClassName('cart__items')[0];
-
+  const priceDiv = document.getElementById('value');
   const sectionProduct = event.target.parentElement;
   const itemID = sectionProduct.getElementsByTagName('span')[0].innerText;
   const { id: sku, title: name, price: salePrice } = await fetchItem(itemID);
   const itemToAdd = createCartItemElement({ sku, name, salePrice });
   OlInCart.appendChild(itemToAdd);
+  saveCartItems(OlInCart.innerHTML);
+  totalPrice += salePrice;
+  priceDiv.innerText = totalPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // formatação encontrada em https://www.alura.com.br;
 };
 
 const createCustomElement = (element, className, innerText) => {
@@ -52,16 +53,23 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
+// para realizar a função abaixo foi consultado o site stackoverflow
 const emptyCart = () => {
+  const priceDiv = document.getElementById('value');
   const cartItems = document.querySelectorAll('.cart__item');
   cartItems.forEach((item) => item.remove());
+  priceDiv.innerText = 0;
 };
 
 const buttonToEmptyCart = document.querySelector('.empty-cart');
 buttonToEmptyCart.addEventListener('click', emptyCart);
 
 window.onload = async () => {
-  const getItemSection = document.querySelector('.items');
+  const getItemsSection = document.querySelector('.items');
+  const createLoading = document.createElement('span');
+  createLoading.className = 'loading';
+  createLoading.innerText = 'carregando...';
+  getItemsSection.appendChild(createLoading);
   const { results } = await fetchProducts('computador');
   results.forEach((item) => {
     const getInfoPruduct = {
@@ -70,6 +78,7 @@ window.onload = async () => {
       image: item.thumbnail,
     };
     const createItem = createProductItemElement(getInfoPruduct);
-    getItemSection.appendChild(createItem);
+    getItemsSection.appendChild(createItem);
   });
+  getItemsSection.firstChild.remove();
 };
