5d300de8e432b135fcbe97232a91ed928943c5df
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-06-10 15:00:54 -0300

    commit pass in the project

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,12 +1,12 @@
 const url = (codigo) => `https://api.mercadolibre.com/items/${codigo}`;
 const fetchItem = async (item) => {
+  if (item === undefined) {
+    throw new Error('You must provide an url');
+   }
 const response = await fetch(url(item));
 const itens = await response.json();
 if (response) {
 return itens;
-}
-if (item === undefined) {
- throw new Error('You must provide an url');
 }
 };
 if (typeof module !== 'undefined') {
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,14 +1,11 @@
 const fetchProducts = async (product) => {
 const url = (busca) => `https://api.mercadolibre.com/sites/MLB/search?q=${busca}`;
+if (product === undefined) {
+  throw new Error('You must provide an url');
+}
  const response = await fetch(url(product));
  const computers = await response.json();
- if (response) {
-     return computers;
-   }
- 
- if (product === undefined) {
-   throw new Error('You must provide an url');
- }
+ return computers;
 };
 
 if (typeof module !== 'undefined') {
