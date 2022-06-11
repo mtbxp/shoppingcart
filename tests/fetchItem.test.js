dc1a1e9145d0ed8fce7eb9ace10cc728f52cf1fc
Matheus <Maths.Aki@gmail.com>
2022-06-11 19:26:59 -0300

    Fazendo a 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,28 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('Testar se é uma função', async () => {
+    const testFunc = typeof await fetchItem();
+    const testResult = 'function';
+    expect(testFunc).toBe(testResult);
+  } );
+  it('Executar a função com o argumento -MLB1615760527- para ver se retorna a fetch', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Testar se o EndPoint ta correto', async () => {
+    await fetchItem('MLB1615760527');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('Testar o retorno da função', async () => {
+    const receberPara = await fetchItem('MLB1615760527');
+    expect(receberPara).toEqual(item);
+    // computadorSearch é chamado no inicio
+  });
+  it('Sem argumento retorna Erro', async () => {
+    const semArgumento = await fetchItem('MLB1615760527');
+    expect(semArgumento).toThrow('You must provide an url');
+  });
 });
