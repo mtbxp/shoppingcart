d51d3e6e1aef1d997b471df4d24df62c68b8c828
Matheus <Maths.Aki@gmail.com>
2022-06-11 19:34:35 -0300

    Tentando fazer a 6, sem saber se esta correta

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,14 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  const testFunc = saveCartItems('<ol><li>Item</li></ol>');
+  const testMeto = localStorage.setItem;
+  it('Teste se, ao executar a função com o argumento -testeFunc- o metodo é chamado', () =>{
+    testFunc;
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Teste se, ao executar a função com o argumento -testFunc-, o metodo é chamado com dois parametros, sendo cartitens e o parametro da função', () => {
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
