9fe0bb9f698c98e7b5178d9faad3f7a0e03fb1db
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-06-11 11:21:27 -0300

    requisito 5 - lint corrigido

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const createProductItemElement = (sku, name, image) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-   event.target.innerHTML = '';
+   event.target.remove();
 };
 
 const createCartItemElement = (sku, name, salePrice) => {
