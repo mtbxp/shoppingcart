64e32c54ffb0963a69412bffd31a98c61958d9c7
Emily Menezes <estudosmenezes@gmail.com>
2022-06-10 21:10:35 -0300

    Finalizando Projeto

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
+// Constantes utilizadas nas funções.
 const getClassCartItems = '.cart__items';
 const getClassItems = document.querySelector('.items');
 const getElement = document.querySelector(getClassCartItems);
 const button = document.querySelector('.empty-cart');
 const getListOl = document.querySelector('ol');
-const addMessage = document.createElement('aside');
-addMessage.innerHTML = 'carregando...';
-addMessage.classList.add('loading');
-getClassItems.appendChild(addMessage);
 
+// Função que calcula o valor total do carrinho
+const getValue = () => {
+  const flag = 0;
+  const message = ' PRICE: $';
+  const getCartItem = document.querySelectorAll('.cart__item');
+  const finishing = document.querySelector('.total-price');
+  const value = [];
+  let amount;
+  getCartItem.forEach((element) => {
+    amount = element.textContent.split(message, flag + 2)[flag + 1];
+    value.push(Number(amount));
+  });
+  const getPrice = value.reduce((i, index) => i + index, flag);
+  finishing.textContent = Number(getPrice.toFixed(2));
+};
+
+// Função que adiciona a mensagem carregando
+const addMessageLoading = () => {
+    const addMessage = document.createElement('aside');
+    addMessage.innerHTML = 'carregando...';
+    addMessage.classList.add('loading');
+    getClassItems.appendChild(addMessage);
+};
+
+// Função que limpa as li, evento que apaga um item
+const cartItemClickListener = (event) => {
+  event.target.closest('li').remove();
+  const getElementOl = document.querySelector(getClassCartItems).innerHTML;
+  saveCartItems(getElementOl);
+   getValue();
+};
+
+// Essa função é responsável por criar a imagem que é exibida
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,6 +45,7 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
+// Essa função é responsável por criar a descrição do produto que é exibido 
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -22,6 +53,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// Essa função é responsável por criar/montar o que será exibido
 const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -35,12 +67,7 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = (event) => {
-  event.target.closest('li').remove();
-  const getElementOl = document.querySelector(getClassCartItems).innerHTML;
-  saveCartItems(getElementOl);
-};
-
+// função que descreve o item com imagem e texto no carrinho de compras
 const createCartItemElement = ({ id: sku, title: name, price: salePrice, thumbnail }) => {
   const li = document.createElement('li');
   const img = document.createElement('img');
@@ -54,25 +81,27 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice, thumbna
   return li;
 };
 
-const addingItems = (flag) => {
-  const elementCart = createCartItemElement(flag);
-  elementCart.addEventListener('click', cartItemClickListener);
-  getElement.appendChild(elementCart);
-};
-
+// Função que adiciona item no carrinho
 const eventItems = () => {
+  const addResponse = (flag) => {
+    const elementCart = createCartItemElement(flag);
+    elementCart.addEventListener('click', cartItemClickListener);
+    getElement.appendChild(elementCart);
+  };
   getClassItems.addEventListener('click', (e) => {
     if (e.target.classList.contains('item__add')) {
       const getCode = getSkuFromProductItem(e.target.parentNode);
       fetchItem(getCode).then((dados) => {
-        addingItems(dados);
+        addResponse(dados);
         const getTagOl = document.querySelector(getClassCartItems).innerHTML;
         saveCartItems(getTagOl);
+         getValue();
       });  
     }
   });
 };
 
+// Função que chama a API com os produtos e remove a mensagem "carregando"
 const createListProducts = async () => {
   const getClassLoading = document.querySelector('.loading');
   const getItems = await fetchProducts('computador');
@@ -83,15 +112,24 @@ const createListProducts = async () => {
   });
 };
 
+// Função que adiciona itens no local Storage
 const addLocalStorage = () => {
   getListOl.innerHTML = getSavedCartItems();
   const getLi = document.querySelectorAll('li');
   getLi.forEach((flag) => flag.addEventListener('click', cartItemClickListener));
+   getValue();
 };
 
+// Função que adiciona evento ao botão de esvaziar o carrinho e limpar o local storage
 button.addEventListener('click', () => {
     localStorage.clear();
     getListOl.innerHTML = '';
+     getValue(); 
 });
 
-window.onload = () => { createListProducts(); eventItems(); addLocalStorage(); };
+window.onload = () => { 
+  addMessageLoading(); 
+  createListProducts(); 
+  eventItems(); 
+  addLocalStorage();
+};
