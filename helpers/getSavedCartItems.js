80b30226d413136cc95061994fb2f7b52e5f41a5
Hugodesousa <hugodesousa.16@gmail.com>
2022-06-11 14:06:15 -0300

    testes para a função getSavedCartItems

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,5 +1,5 @@
 const getSavedCartItems = () => {
-  const test = JSON.parse(localStorage.getItem('cartItems'));
+  const test = localStorage.getItem('cartItems');
   return test;
 };
 if (typeof module !== 'undefined') {
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,13 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa se getSavedCartItems é uma função', () => {
+    expect(typeof getSavedCartItems).toBe('function')
+  });
+
+  test('Teste se, ao executar getSavedCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    getSavedCartItems();
+
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
 });
