7077e8bd412bc49c87ae166f389654bcec57468b
Henrique Rosa <samuel_kuik@hotmail.com>
2022-06-11 17:35:08 -0300

    entregue Requisito 9

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let totalPrice = 0;
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -6,9 +5,23 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
+const cartPriceManagement = () => {
+  const priceDiv = document.getElementById('value');
+  let totalPrice = 0;
+  const liInOlCart = document.querySelectorAll('.cart__item');
+  liInOlCart.forEach((item) => {
+    const numberOfInner = Number(item.innerText.split(' ').pop().slice(1));
+    totalPrice += numberOfInner;
+  });
+  // const formatNumber = totalPrice;
+  priceDiv.innerText = totalPrice;
+   /*  .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // formatação encontrada em https://www.alura.com.br; */
+};
+
 const cartItemClickListener = (event) => {
   // coloque seu código aqui;
   event.target.remove();
+  cartPriceManagement();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -22,15 +35,13 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
 
 const addItemToCart = async (event) => {
   const OlInCart = document.getElementsByClassName('cart__items')[0];
-  const priceDiv = document.getElementById('value');
   const sectionProduct = event.target.parentElement;
   const itemID = sectionProduct.getElementsByTagName('span')[0].innerText;
   const { id: sku, title: name, price: salePrice } = await fetchItem(itemID);
   const itemToAdd = createCartItemElement({ sku, name, salePrice });
   OlInCart.appendChild(itemToAdd);
   saveCartItems(OlInCart.innerHTML);
-  totalPrice += salePrice;
-  priceDiv.innerText = totalPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // formatação encontrada em https://www.alura.com.br;
+  cartPriceManagement();
 };
 
 const createCustomElement = (element, className, innerText) => {
@@ -55,10 +66,9 @@ const createProductItemElement = ({ sku, name, image }) => {
 
 // para realizar a função abaixo foi consultado o site stackoverflow
 const emptyCart = () => {
- // const priceDiv = document.getElementById('value');
-  const cartItems = document.querySelectorAll('.cart__item');
-  cartItems.forEach((item) => item.remove());
- // priceDiv.innerText = 0;
+   const cartItems = document.querySelectorAll('.cart__item');
+   cartItems.forEach((item) => item.remove());
+   cartPriceManagement();
 };
 
 const buttonToEmptyCart = document.querySelector('.empty-cart');
