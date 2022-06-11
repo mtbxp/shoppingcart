b38529ed70d6c4907cc1a9929b1491fbfb5381b6
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-06-11 09:17:14 -0300

    requisito 4

--- script.js
+++ script.js
@@ -20,34 +20,45 @@ const createProductItemElement = (sku, name, image) => {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+   // codigo
 };
 
-const createCartItemElement = ({ sku, name, salePrice }) => {
+const createCartItemElement = (sku, name, salePrice) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.appendChild(createProductImageElement(image));
   li.addEventListener('click', cartItemClickListener);
   return li;
+};
+
+const cart = async (event) => {
+  const carrinho = document.querySelector('.cart__items');
+  const selected = event.target.parentElement.firstElementChild.innerText;
+  // const img = event.target.parentElement.firstElementChild.nextElementSibling.nextElementSibling.src;
+  const itemJSON = await fetchItem(selected);
+  const { id, title, price } = itemJSON;
+  const itemCarrinho = createCartItemElement(id, title, price);
+  carrinho.appendChild(itemCarrinho);
 };
 
 const renderProducts = async () => {
   const items = document.getElementsByClassName('items')[0];
   const products = await fetchProducts('computador');
   const produto = products.results;
-  console.log(produto);
   produto.forEach((element) => {
     const { id, title, thumbnail } = element;
-    const countrycard = createProductItemElement(id, title, thumbnail);
-    items.appendChild(countrycard);
-  });
+    const cardProduto = createProductItemElement(id, title, thumbnail);
+    items.appendChild(cardProduto);
+    });
+  items.addEventListener('click', cart);
 };
 
 window.onload = () => {
