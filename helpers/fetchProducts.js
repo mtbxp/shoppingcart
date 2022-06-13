3e7464bd5c69f991b0a8f2876e0b087e7f577f10
bigJhow88 <jhow.and@gmail.com>
2022-06-13 11:26:11 -0300

    corrigindo requito 1, estava errado, mas tive ajuda e foi

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,5 @@
-const fetchProducts = async ($QUERY) => {
-  if ($QUERY === undefined) {
-    return 'You must provide an url';
-  } if (typeof $QUERY === 'string') {
-    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  }
+const fetchProducts = () => {
+  // seu código aqui
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,9 +3,24 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  it('A function', async () => {
-    const result = fetchProducts;
-    
-    expect(typeof result).toBe('function');
-  });
+ it (`'Testar se fetchProducts é uma função'`, () => {
+  expect (typeof fetchProducts).toBe('function');
+ } );
+ it (`Execute a função com argumento 'computador' para retornar fetch`, async () => {
+   await fetchProducts('computador');
+  expect(fetch).toHaveBeenCalledWidth();
+ });
+ it (`Testar se o EndPoint está correto`, async () => {
+   await fetchProducts('computador');
+   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  expect(fetch).toHaveBeenCalledWidth(endpoint);
+ });
+ it (`Testar o retorno da função`, async () => {
+   const receivedFor = await fetchProducts('computador');
+  expect(receivedFor).toEqual(computadorSearch);
+ });
+ it (`Sem argumento retorna Error`, async () => {
+   const noArgument = fetchProducts.toThrow();
+  await expect(noArgument).rejects.toThrow(new Error('You must provide an url'));
+ });
 });
