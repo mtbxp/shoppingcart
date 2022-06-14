41bb80c585ca01cfaa38e4cc4b753bf20f87bf4e
Ravanz <rayteon.h@gmail.com>
2022-06-14 16:55:39 -0300

    requisito 7 concluído

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,17 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('ao executar a função getSavedCartItems o método `localStorage.setItem` deve ser chamado.', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+
+  it('ao executar a função getSavedCartItems, o método `localStorage.getItem` deve ser chamado com o paramentro `cartItems`.', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith(localStorage.getItem('cartItems'));
+  })
+  it('deve retornar uma mensagem de erro `mensagem esperada aqui` caso não receba argumento', async () =>{
+    ;
+    expect(await getSavedCartItems()).toEqual(new Error('mensagem esperada aqui'));
+    });
 });
