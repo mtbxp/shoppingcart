b9d6dfa78d3d3ce6a58cbdbd3d90f1fc9880e231
Ravanz <rayteon.h@gmail.com>
2022-06-14 16:45:35 -0300

    requisito 6 concluído

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,17 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('ao executar a função saveCartItems com argumento `<ol><li>Item</li></ol>`, o método `localStorage.setItem` deve ser chamado.', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+
+  it('ao executar a função saveCartItems com o argumento `<ol><li>Item</li></ol>`, o método `localStorage.setItem` deve ser chamado com 2 paramentros, sendo o primeiro `cartItems` e o segundo `<ol><li>Item</li></ol>`.', async () => {
+    await saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith(localStorage.setItem('cartItems', '<ol><li>Item</li></ol>'));
+  })
+  it('deve retornar uma mensagem de erro `mensagem esperada aqui` caso não receba argumento', async () =>{
+    ;
+    expect(await saveCartItems()).toEqual(new Error('mensagem esperada aqui'));
+    });
 });
