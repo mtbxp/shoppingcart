3e594ae32a07b7141fed206632645de304d0b117
Luis Alves <coopercontato27@gmail.com>
2022-06-14 20:31:37 -0300

    emplementando requisitos 4 acima

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sectionItems = document.querySelector('.items');
+const sectionCartItems = document.querySelector('.cart__items');
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -26,20 +27,40 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
   return section;
 };
 
-const renderItems = () => {
-  fetchProducts('computador')
-    .then((retorno) => retorno.results
-      .forEach((produto) => sectionItems
-        .appendChild(createProductItemElement(produto))));
-};
-
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+const itemsToStorage = () => {
+  const textToBeSaved = sectionCartItems.innerHTML;
+  saveCartItems(textToBeSaved);
 };
 
-const createCartItemElement = ({ sku, name, salePrice }) => {
+const sumCartItems = () => {
+  let sum = 0;
+  const totalPrice = document.querySelector('.total-price');
+  const lis = document.querySelectorAll('li');
+  
+  lis.forEach((item) => {
+    sum += parseFloat(item.innerText.split('$')[1] * 100);
+  });
+  totalPrice.innerHTML = (sum / 100);
+};
+
+const cartItemClickListener = (event) => {
+  event.target.remove();
+  itemsToStorage();
+  sumCartItems();
+};
+
+const loadLocalData = () => {
+  const itemsSalvos = getSavedCartItems();
+  sectionCartItems.innerHTML = itemsSalvos;
+  const lis = document.querySelectorAll('li');
+  lis.forEach((item) => {
+    item.addEventListener('click', cartItemClickListener);
+  });
+};
+
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -47,7 +68,43 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
+const renderItems = () => {
+  fetchProducts('computador')
+    .then((retorno) => retorno.results
+      .forEach((produto) => sectionItems
+        .appendChild(createProductItemElement(produto))));
+};
+
+const addition = (info) => {
+  sectionCartItems.appendChild(createCartItemElement(info));
+  sumCartItems();
+};
+
+const addEventToProducts = async (event) => {
+  const id = getSkuFromProductItem(event.target.parentNode);
+  await fetchItem(id).then((data) => addition(data));
+  itemsToStorage();
+};
+
+const addItemsToCart = () => {
+  sectionItems.addEventListener('click', addEventToProducts);
+};
+
+const esvaziaCarrinho = () => {
+  const lis = document.querySelectorAll('.cart__item');
+  lis.forEach((item) => item.remove());
+  localStorage.clear('cartItems');
+  sumCartItems();
+};
+
+const addEventToBtn = () => {
+  const button = document.querySelector('.empty-cart');
+  button.addEventListener('click', esvaziaCarrinho);
+};
+
 window.onload = () => {
-  fetchProducts('computador');
+  addItemsToCart();
+  loadLocalData();
+  addEventToBtn();
   renderItems();
 };
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -12,19 +12,19 @@ describe('2 - Teste a função fetchItem', () => {
     expect(fetch).toHaveBeenCalledTimes(1);
   });
 
-  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";', () => {
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";',async () => {
     const url = "https://api.mercadolibre.com/items/MLB1615760527"
-    fetchItem("MLB1615760527");
+    await fetchItem("MLB1615760527");
     expect(fetch).toHaveBeenCalledWith(url);
   });
 
-  it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', () => {
-    const result = fetchItem("MLB1615760527");
+  it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.',async () => {
+    const result =await fetchItem("MLB1615760527");
     expect(result).toEqual(item);
   });
 
-  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url".', () => {
-    const result = fetchItem();
+  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url".',async () => {
+    const result = await fetchItem();
     expect(result).toEqual(new Error('You must provide an url'))
   });
 });
