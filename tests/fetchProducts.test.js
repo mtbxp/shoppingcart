cfc01df4f3693082dedc61802a6dc467b380a45d
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-14 22:02:36 -0300

    Acredito que o primeiro requisito feito

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,21 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
-});
+  
+  test('Se a função fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe("function");
+  });
+
+  // test('Se função fetchProducts com o argumento computador e teste se fetch foi chamada', async () => {
+  //   await fetchProducts('computador');
+  //   expect(fetch).toBeCalled();
+  // });
+
+  // test('Testa se  ao chamar a função argumento "computador", a função fetch utiliza o endpoint ""', async () => {
+  //   await fetchProducts('computador');
+  //     expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  //   })
+  // //fail('Teste vazio');
+})
+
+// npm test fetchProducts
