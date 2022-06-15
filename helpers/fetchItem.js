94c03488d882ad65d6d14db848fa44f1907ce6b2
Melania P Chagas <melaniapchagas@gmail.com>
2022-06-15 17:47:18 -0300

    Requisito 4: implementando a funcionalidade 'adicionar ao carrinho'

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
-};
+async function fetchItem(itemId) {
+  try {
+    const data = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+    return await data.json();
+  } catch (error) {
+      return error;
+  }
+}
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const itemsSection = document.querySelector('.items');
-
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -32,7 +30,8 @@ const cartItemClickListener = (event) => {
   // coloque seu código aqui
 };
 
-const createCartItemElement = ({ sku, name, salePrice }) => {
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
+  console.log(sku, name, salePrice);
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,12 +39,36 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-async function appendItemsInSection() {
-  const data = await fetchProducts('computador');
-  data.results.forEach((product) => {
-    itemsSection.appendChild(createProductItemElement(product));
+async function addToCart(event) {
+  const clickedProduct = await fetchItem(event.target.id);
+  const li = createCartItemElement(clickedProduct);
+  document.querySelector('.cart__items').appendChild(li);
+}
+
+function addingIdsInAddToCartButtons(products) {
+  const buttons = Array.from(document.getElementsByClassName('item__add'));
+  products.forEach((product, indice) => { 
+    buttons[indice].id = product.id;
   });
 }
-window.onload = () => { 
-  appendItemsInSection();
+
+function addingEventListenerInAddToCartButtons() {
+  // Objetos array-like são retornados de muitos métodos DOM nativos como getElementsByClassName(),por isso foi necessário usar o método 'Array.from' para convertê-lo em um array.
+  Array.from(document.getElementsByClassName('item__add'))
+  .forEach((button) => button.addEventListener('click', addToCart)); 
+}
+
+function appendItemsInSection(products) {
+  const itemSection = document.querySelector('.items');
+  products.forEach((product) => {
+    itemSection.appendChild(createProductItemElement(product));
+  });
+}
+
+window.onload = async () => {
+  const data = await fetchProducts('computador');
+  const products = data.results;
+  appendItemsInSection(products);
+  addingEventListenerInAddToCartButtons();
+  addingIdsInAddToCartButtons(products);
 };
