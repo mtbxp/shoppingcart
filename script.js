a8477cfe1eedd53231810883ba474bb23163f060
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-16 18:32:17 -0300

    agora testando 2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,6 +22,15 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
+};
+
+const computersAvaible = async () => {
+  const arrayComputers = await fetchProducts('computer');
+  const wantedHTMLelement = document.querySelector('.items');
+  arrayComputers.results.forEach((eachComputer) => {
+    const append = createProductItemElement(eachComputer);
+    wantedHTMLelement.appendChild(append);
+  });
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
@@ -38,4 +47,6 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => {
+ computersAvaible(); 
+};
