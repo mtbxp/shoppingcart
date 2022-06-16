3aee5027d7d4aeb5f1767893a908b6e83a45a5f6
Camila <falaschi.camila@yahoo.com.br>
2022-06-16 18:20:38 -0300

    Retirando async-await do teste saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,10 +5,10 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // Para os testes abaixo foi recebido a orientação do instrutor Victor Felipe Ramos Saraiva da Trybe
-  test('se ao executar a função com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', async () => {
+  test('se ao executar a função com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
     // para 'localStorage.setItem' foi consultado o site (https://www.npmjs.com/package/jest-localstorage-mock)
     const actual = '<ol><li>Item</li></ol>';
-    await saveCartItems(actual);
+    saveCartItems(actual);
     expect(localStorage.setItem).toBeCalled();
   });
   test('se ao executar a função com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros', () => {
