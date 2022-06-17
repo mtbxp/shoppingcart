c5a25cbef8b4abec53875139f49053dee11404ed
Fernando Costa <fernando.barreto.costa@gmail.com>
2022-06-17 12:22:27 -0300

    Finalizado requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // const { fetchProducts } = require('./helpers/fetchProducts');
 // const { fetchItem } = require('./helpers/fetchItem');
 
+const cartItems = document.getElementsByClassName('cart__items')[0];
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,18 +17,22 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+const cartItemClickListener = (event) => {
+  const itemToBeRemoved = event.target;
+  cartItems.removeChild(itemToBeRemoved);
+};
+
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 };
 
 const addToCart = async ({ target }) => {
   const itemToBeFetched = target.parentNode.firstChild.innerText;
   const itemToBeAdded = await fetchItem(itemToBeFetched);
-  const cartItems = document.getElementsByClassName('cart__items')[0];
   cartItems.appendChild(createCartItemElement(itemToBeAdded));
 };
 
@@ -45,10 +51,6 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 };
 
 // const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-// const cartItemClickListener = (event) => {
-//   event.target
-// };
 
 const itemsSection = document.getElementsByClassName('items')[0];
 
