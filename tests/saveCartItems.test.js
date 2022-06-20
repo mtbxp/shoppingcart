d9b6d76bf5acc4a92d3843b5ed518bca2e7cf1bb
Cesar Xavier <cxavier.vet@gmail.com>
2022-06-20 00:17:32 -0300

    Requisito 6: Implementando testes na saveCartItems e revisao de storage

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,14 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+  it('Testa se o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('Testa localStorage é chamado com 2 parâmetros', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+
+  });
 });
