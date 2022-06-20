f9439908a2c441269a511a6870d1872f8fdbf7e0
Cesar Xavier <cxavier.vet@gmail.com>
2022-06-20 10:05:29 -0300

    Requisito 6: Corrigindo o lint da função saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,14 +4,14 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  
-  it('Testa se o método localStorage.setItem é chamado', () => {
+
+  it('Testa se o método localStorage.setItem é chamado', async () => {
     saveCartItems('<ol><li>Item</li></ol>');
-    expect(localStorage.setItem).toBeCalled();
+    expect(await localStorage.setItem).toBeCalled();
   });
-  it('Testa localStorage é chamado com 2 parâmetros', () => {
+  it('Testa localStorage é chamado com 2 parâmetros', async () => {
     saveCartItems('<ol><li>Item</li></ol>');
-    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+    expect(await localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
 
   });
 });
