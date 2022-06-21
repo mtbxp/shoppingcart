9c027b7492f93f0787e5636c723ee71846c51552
Eduardo Dias <85201931+TauraEdu@users.noreply.github.com>
2022-06-21 12:56:30 -0300

    Implementação dos teste item 03.

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,22 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Teste se fetch é chamado ao executar a função fetchItem', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Teste se fetch usa endpoint específico ao executar a função fetchItem', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('Teste se retorna o item correto', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('Teste se retorna erro quando fetchItem é passado sem argumentos ', async () => {
+    expect(await fetchItem()).toEqual(new Error ('You must provide an url'));
+  });
+  
 });
