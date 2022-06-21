e3fcf86f23440f532f187f83b7b0feb7dcf56636
Melania P Chagas <melaniapchagas@gmail.com>
2022-06-21 20:14:41 -0300

    Corrigindo o requisito 6

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -2,18 +2,13 @@ const saveCartItems = (clickedItem) => {
    // Ao receber dados de um servidor web, os dados são sempre uma string. Analise os dados com JSON.parse() e os dados se tornam um objeto JavaScript(www.w3schools.com/js/js_json_parse.asp)
   // LS = Local Storage
   // console.log(clickedItem);
-  const itemsSavedInLS = JSON.parse(localStorage.getItem('cartItems'));
-  let itemsToSaveInLS = [];
-  if (itemsSavedInLS === null) {
-    itemsToSaveInLS.push(clickedItem);
-  } else {
-    itemsSavedInLS.push(clickedItem);
-    itemsToSaveInLS = itemsSavedInLS;
-  }
+  // const itemsSavedInLS = localStorage.getItem('cartItems');
+  const itemsToSaveInLS = 0; /* itemsSavedInLS === null ? [clickedItem] 
+  : itemsSavedInLS.concat(clickedItem); */
 
   // O método JSON.stringify() converte valores em javascript para uma String  JSON. 
   // developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
-  localStorage.setItem('cartItems', JSON.stringify(itemsToSaveInLS));
+  localStorage.setItem('cartItems', itemsToSaveInLS);
 };
 
 if (typeof module !== 'undefined') {
