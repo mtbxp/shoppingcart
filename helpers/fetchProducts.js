14d087a679b9fea3f1ee368b2760f65906b63cf1
Bambirra <ivan.bambirra@engenharia.ufjf.br>
2022-06-22 19:29:53 -0300

    nao pode ser

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,7 +4,6 @@ const fetchProducts = async (products) => {
     const ENDPOINT = `https://api.mercadolibre.com/sites/MLB/search?q=${products}`;
     const apiResponse = await fetch(ENDPOINT);
     const productData = await apiResponse.json();
-    console.log(productData.results.length);
     return productData;
   };
   
--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,6 +1,6 @@
 const getSavedCartItems = () => {
-  const guardCart = localStorage.getItem('cartItems');
-  document.querySelector('.cart__items').innerHTML = guardCart;
+  localStorage.getItem('cartItems');
+  // document.querySelector('.cart__items').innerHTML = guardCart;
 };
 
 if (typeof module !== 'undefined') {
