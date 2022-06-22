42804a5edd071501c1964f9b267309ea46841c93
Roberto <robertog.adm@gmail.com>
2022-06-22 14:09:49 -0300

    Arruma o teste getSavedCartItem

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -3,14 +3,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  test('ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', () => {
-    await getSavedCartItems();
-    expect(localStorage.getItem).toBeCalled();
+describe('4 - Teste a função getSavedCartItems', async () => {
+  test(' é realmente é uma função', () => {
+    expect(typeof getSavedCartItems).toBe('function');
   });
 
   test('ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro cartItems e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
-    await getSavedCartItems();
-    expect(localStorage.getItem).toBeCalledWith('cartItems');
+    expect.assertions(2);
+
+    expect(localStorage.getItem).toHaveBeenCalled();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   })
 });
