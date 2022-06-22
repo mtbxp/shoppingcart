26ae409752c445c9129c4226333f49731d3791a2
Anderson Jaco <andersoncoelho2010@hotmail.com>
2022-06-21 22:16:58 -0300

    requisito 11

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').inn
 const cartItem = document.getElementsByClassName('cart__items')[0];
 const butRemov = document.getElementsByClassName('empty-cart')[0];
 
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -81,7 +82,22 @@ const removeItemFromCart = () => {
   });
 };
 
+function startLoading() {
+  const eleDom = document.createElement('div');
+  eleDom.className = 'loading';
+  eleDom.textContent = 'carregando...';
+}
+
+function clearLoading() {
+  const eleDom = document.getElementsByClassName('.loading')[0];
+  eleDom.remove();
+}
+
 window.onload = () => {
-  sendProduct();
   removeItemFromCart();
+  startLoading();
+  setTimeout(() => {
+    sendProduct();
+    clearLoading();
+  }, 1000);
  };
