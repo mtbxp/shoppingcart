618e213045c9eb02aee67e4a095728dd88432bed
Raiane Albergaria <raiane.vian@gmail.com>
2022-06-22 16:07:02 -0300

    Requisito 3 realizado

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  if (typeof id === 'undefined') return (new Error('You must provide an url'));
+  const url = `https://api.mercadolibre.com/items/${id}`;
+  const promise = await fetch(url);
+  const result = await promise.json();
+  console.log(result);
+  return (result);
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,24 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Check if \'fetchItem\' is a function.', async () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+  it('Testa se ao chamar com argumento \'MLB1615760527\' a fetch é chamada.', async () => {
+    await fetchItem('MLB1615760527');
+      expect(fetch).toHaveBeenCalled();
+    });
+  it('Testa se ao chamar com argumento \'MLB1615760527\' o endpoint é correto', async () => {
+    await fetchItem('MLB1615760527');
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  it('Checks if using computer as parameter returns the correct object.', async () => {
+    const data = await fetchItem('MLB1615760527');
+    expect(data).toEqual(item);
+  });
+  it('Check if it returns \'You must provide an url\' when executing the function with parameter that does not exist.', async () => {
+    const data = await fetchItem();
+    expect(data).toEqual(new Error('You must provide an url'))
+  });
 });
